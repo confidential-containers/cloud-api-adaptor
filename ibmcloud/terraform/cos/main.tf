b07bc4d0ec346fd44721894d6113685519f316e2
[Jonah-Farrow] <jonah.farrow@ibm.com>
2022-05-16 16:15:15 +0100

    automation: TF template to create COS resources
    Work completed to automate COS creation to store image from Build a
    pod VM image section of terraform/README.md.
    The main.tf file creates the instances.
    This automation allows the user to run a Terraform apply command
    to create all the storage required to store an image.
    
    Fixes: #17
    
    Signed-off-by: [Jonah-Farrow] <jonah.farrow@ibm.com>

---
+++ ibmcloud/terraform/cos/main.tf
@@ -0,0 +1,29 @@
+data "ibm_resource_group" "group" {
+  name = "default"
+}
+
+########## Create a COS instance
+resource "ibm_resource_instance" "cos_instance" {
+  name              = var.cos_service_instance_name
+  service           = "cloud-object-storage"
+  plan              = "standard"
+  location          = "global"
+  resource_group_id = data.ibm_resource_group.group.id
+}
+
+####### Create IAM Authorization Policy
+resource "ibm_iam_authorization_policy" "policy" {
+    source_service_name  = "is"
+    source_resource_type = "image"
+    target_service_name  = "cloud-object-storage"
+    target_resource_instance_id = ibm_resource_instance.cos_instance.guid
+    roles                = ["Reader"]
+}
+
+######## Create COS bucket
+resource "ibm_cos_bucket" "state_bucket" {
+  bucket_name          = var.cos_bucket_name
+  resource_instance_id = ibm_resource_instance.cos_instance.id
+  region_location      = var.ibm_region
+  storage_class        = "standard"
+}
