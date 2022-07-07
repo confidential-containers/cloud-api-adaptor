ea3430741d28cfc23cd20c43764d98659ca0ce12
James Tumber <james.tumber@ibm.com>
2022-07-07 07:28:09 -0700

    ibmcloud: provider version upgrade (1.43.0)
    terraform modules updated:
    - [x] root
    - [x] check-podvm-instance
    - [x] cluster
    - [x] common
    - [x] cos
    - [x] podvm-build
    - [x] run-nginx-demo
    - [x] start-cloud-api-adaptor
    
    Fixes: #76
    
    Signed-Off-By: James Tumber <james.tumber@ibm.com>

--- ibmcloud/terraform/cluster/versions.tf
+++ ibmcloud/terraform/cluster/versions.tf
@@ -2,7 +2,7 @@ terraform {
     required_providers {
         ibm = {
             source = "IBM-Cloud/ibm"
-            version = "~> 1.34.0"
+            version = "~> 1.43.0"
         }
     }
 }
