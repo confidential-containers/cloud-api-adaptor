0bc576e6c9e35ec9b7ad21dcb8abdef51099b99a
Dave Hay <david_hay@uk.ibm.com>
2022-07-15 18:09:55 +0100

    ibmcloud: Obfuscate API key in log file
    Extend types.go to obfuscate IBM Cloud API key in cloud-api-adaptor.log
    
    Fixes #83
    
    Signed-off-by: Dave Hay <david_hay@uk.ibm.com>

--- pkg/adaptor/hypervisor/ibmcloud/types.go
+++ pkg/adaptor/hypervisor/ibmcloud/types.go
@@ -1,5 +1,15 @@
 package ibmcloud
 
+import (
+	"fmt"
+	"reflect"
+	"sort"
+)
+
+const (
+	keyMask = "********"
+)
+
 type Config struct {
 	ApiKey                   string
 	IamServiceURL            string
@@ -14,4 +24,60 @@ type Config struct {
 	SecondarySecurityGroupID string
 	KeyID                    string
 	VpcID                    string
+}
+
+var configFields []string
+
+// Use reflect to manage list of fields in the Config struct
+func init() {
+	typeOf := reflect.TypeOf(Config{})
+	configFields = make([]string, typeOf.NumField())
+	for i := 0; i < typeOf.NumField(); i++ {
+		configFields[i] = typeOf.Field(i).Name
+	}
+	sort.Strings(configFields)
+}
+
+// Add Stringer interface to return keyMask when outputting ApiKey field
+func (config *Config) String() string {
+	apikey := config.ApiKey
+	if apikey != "" {
+		apikey = keyMask
+	}
+	return fmt.Sprintf(apikey)
+}
+
+// Add custom Formatter to handle different output cases
+// Also return keyMask if the field name is ApiKey
+func (config Config) Format(state fmt.State, verb rune) {
+	switch verb {
+	case 's', 'q':
+		val := config.String()
+		if verb == 'q' {
+			val = fmt.Sprintf("%q", val)
+		}
+		fmt.Fprint(state, val)
+	case 'v':
+		if state.Flag('#') {
+			fmt.Fprintf(state, "%T", config)
+		}
+		fmt.Fprint(state, "{")
+		val := reflect.ValueOf(config)
+		for i, name := range configFields {
+			if state.Flag('#') || state.Flag('+') {
+				fmt.Fprintf(state, "%s:", name)
+			}
+			fld := val.FieldByName(name)
+			// If the field is ApiKey, apply the keyMask
+			if name == "ApiKey" {
+				fmt.Fprint(state, keyMask)
+			} else {
+				fmt.Fprint(state, fld)
+			}
+			if i < len(configFields)-1 {
+				fmt.Fprint(state, " ")
+			}
+		}
+		fmt.Fprint(state, "}")
+	}
 }
