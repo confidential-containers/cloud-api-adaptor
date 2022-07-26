3f3f055a9b4cc95f3be1412b8cb18e92c849dd29
Pradipta Banerjee <pradipta.banerjee@gmail.com>
2022-07-26 17:40:36 +0000

    deploy: Add artifacts to create runtime payload container image
    Fixes: #121
    
    Signed-off-by: Pradipta Banerjee <pradipta.banerjee@gmail.com>

---
+++ install/runtime-payload/scripts/run-aws.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+/opt/confidential-containers/bin/cloud-api-adaptor-aws aws \
+    -aws-access-key-id ${AWS_ACCESS_KEY_ID} \
+    -aws-secret-key ${AWS_SECRET_ACCESS_KEY} \
+    -aws-region ${AWS_REGION} \
+    -pods-dir /run/peerpod/pods \
+    -socket /run/peerpod/hypervisor.sock 
