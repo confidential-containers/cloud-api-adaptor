a8e574b38f6fe7fa7234b02da7dfe97ca9403bce
Pradipta Banerjee <pradipta.banerjee@gmail.com>
2022-09-15 12:56:30 +0000

    install: Update runtime payload image with latest kata runtime changes
    Fixes: #231
    
    Signed-off-by: Pradipta Banerjee <pradipta.banerjee@gmail.com>

--- install/runtime-payload/hack/binaries.sh
+++ install/runtime-payload/hack/binaries.sh
@@ -7,28 +7,15 @@ set -o nounset
 script_dir=$(dirname "$(readlink -f "$0")")
 bin_dir=${script_dir}/../bin
 kata_dir=${script_dir}/../../../../kata-containers
-providers=( aws ibmcloud libvirt )
-
-function build_cloud_api_adaptor() {
-	pushd "${script_dir}/../../.."
-        CLOUD_PROVIDER=$1 make clean
-        CLOUD_PROVIDER=$1 make
-	cp cloud-api-adaptor ${bin_dir}/cloud-api-adaptor-$1 
-	popd
-}
 
 function build_kata_runtime() {
 	pushd "${kata_dir}/src/runtime"
         make
-	cp containerd-shim-kata-v2 kata-monitor kata-runtime ${bin_dir}/ 
+	cp containerd-shim-kata-v2 ${bin_dir}/ 
 	popd
 }
 function main() {
 	mkdir -p ${bin_dir}
-	for i in "${providers[@]}"
-	do
-		build_cloud_api_adaptor $i
-	done
 	build_kata_runtime
 }
 
