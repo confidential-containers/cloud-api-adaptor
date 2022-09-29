5f1c65fa4a9cbb87ce647d501c6c288fb67e3207
Yohei Ueda <yohei@jp.ibm.com>
2022-09-29 15:28:31 +0900

    adaptor: Correct pod names reported by cri-o
    cri-o reports sandbox names in the different
    format than containerd.
    
    This patch corrects pod names reported by cri-o so that
    they are consistent with containerd.
    
    Fixes #261
    
    Signed-off-by: Yohei Ueda <yohei@jp.ibm.com>

---
+++ pkg/util/hvutil/hvutil.go
@@ -0,0 +1,25 @@
+package hvutil
+
+import (
+	"strings"
+
+	cri "github.com/containerd/containerd/pkg/cri/annotations"
+)
+
+func GetPodName(annotations map[string]string) string {
+
+	sandboxName := annotations[cri.SandboxName]
+
+	// cri-o stores the sandbox name in the form of k8s_<pod name>_<namespace>_<uid>_0
+	// Extract the pod name from it.
+	if tmp := strings.Split(sandboxName, "_"); len(tmp) > 1 && tmp[0] == "k8s" {
+		return tmp[1]
+	}
+
+	return sandboxName
+}
+
+func GetPodNamespace(annotations map[string]string) string {
+
+	return annotations[cri.SandboxNamespace]
+}
