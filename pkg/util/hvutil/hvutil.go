3e6a27a2e3405c838ecdaa22a7dd55d05b9adbbc
Yohei Ueda <yohei@jp.ibm.com>
2022-10-07 15:34:24 +0900

    util: Add CreateInstanceName function
    This function generates a VM instance name from
    sanitized values of node name, pod namespace, pod
    name, and sandbox ID.
    
    Signed-off-by: Yohei Ueda <yohei@jp.ibm.com>

--- pkg/util/hvutil/hvutil.go
+++ pkg/util/hvutil/hvutil.go
@@ -1,6 +1,7 @@
 package hvutil
 
 import (
+	"fmt"
 	"strings"
 
 	cri "github.com/containerd/containerd/pkg/cri/annotations"
@@ -22,4 +23,30 @@ func GetPodName(annotations map[string]string) string {
 func GetPodNamespace(annotations map[string]string) string {
 
 	return annotations[cri.SandboxNamespace]
+}
+
+func sanitize(input string) string {
+
+	var output string
+
+	for _, c := range strings.ToLower(input) {
+		if !(('a' <= c && c <= 'z') || ('0' <= c && c <= '9') || c == '-') {
+			c = '-'
+		}
+		output += string(c)
+	}
+
+	return output
+}
+
+func CreateInstanceName(nodeName, podNamespace, podName, sandboxID string) string {
+
+	nodeName = sanitize(nodeName)
+	podNamespace = sanitize(podNamespace)
+	podName = sanitize(podName)
+	sandboxID = sanitize(sandboxID)
+
+	vmName := fmt.Sprintf("podvm-%s-%s-%s-%.8s", nodeName, podNamespace, podName, sandboxID)
+
+	return vmName
 }
