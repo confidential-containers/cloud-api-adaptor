d1ce1bc002467ff956e0133a1548f2577cee3f24
stevenhorsman <steven@uk.ibm.com>
2022-11-04 21:46:20 +0000

    aws: Remove sig verification
    Add agent image section to stop signature verification
    being enabled
    
    Fixes: #331
    Signed-off-by: stevenhorsman <steven@uk.ibm.com>

--- aws/image/copy-files.sh
+++ aws/image/copy-files.sh
@@ -1,4 +1,5 @@
 sudo mkdir -p /etc/containers
+sudo cp /tmp/files/etc/agent-config.toml /etc/agent-config.toml
 sudo cp -a /tmp/files/etc/containers/* /etc/containers/
 sudo cp -a /tmp/files/etc/systemd/* /etc/systemd/
 
