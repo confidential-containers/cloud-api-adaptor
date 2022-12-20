a19e917a59b7bdfbbbb96cd897bdb2314fa8c186
Snir Sheriber <ssheribe@redhat.com>
2022-12-20 10:08:56 +0200

    image: support aa-offline_fs_kbc-resources.json placing
    so that it will be copied if provided at image build
    
    Signed-off-by: Snir Sheriber <ssheribe@redhat.com>

--- podvm/qcow2/copy-files.sh
+++ podvm/qcow2/copy-files.sh
@@ -5,6 +5,9 @@ sudo mkdir -p /etc/containers
 sudo cp /tmp/files/etc/agent-config.toml /etc/agent-config.toml
 sudo cp -a /tmp/files/etc/containers/* /etc/containers/
 sudo cp -a /tmp/files/etc/systemd/* /etc/systemd/
+if [ -e /tmp/files/etc/aa-offline_fs_kbc-resources.json ]; then
+	sudo cp /tmp/files/etc/aa-offline_fs_kbc-resources.json /etc/aa-offline_fs_kbc-resources.json
+fi
 
 
 sudo mkdir -p /usr/local/bin
