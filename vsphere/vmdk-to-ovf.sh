aa5605bcac0d1e1c17e20ae1f8cce132adf752ef
Bandan Das <bsd@redhat.com>
2023-01-11 01:27:31 -0500

    vsphere: vmdk-to-ovf.sh: add a force option
    This option overwrites an existing template by making sure
    to delete template files, if they exist, before the current run.
    
    The default is yes - always assume a force install.
    
    Fixes: #506
    Signed-off-by: Bandan Das <bsd@redhat.com>

--- vsphere/vmdk-to-ovf.sh
+++ vsphere/vmdk-to-ovf.sh
@@ -22,12 +22,14 @@ VCENTER_DATASTORE=$5
 # Change this to 0 to enable ssl verification
 IGNORE_SSL=1
 
+# Change this to 0 to not overwrite an existing template
+FORCE_INSTALL=1
+
 # The name of the uploaded template
 TEMPLATE_NAME=$6
 
 # Path to the vmdk to convert to a template
 VMDK_PATH=$7
-
 
 TMPDIR=$(mktemp -d)
 IMAGE_NAME="$(basename -- ${VMDK_PATH})"
@@ -44,16 +46,14 @@ pre_checks() {
         [[ -x "$(command -v pwsh)" ]] || { warnmsg "Powershell is not installed, Please see: https://learn.microsoft.com/en-us/powershell/scripting/install/installing-powershell-on-linux?view=powershell-7.3" exit 1; }
 	(pwsh -Command Get-InstalledModule -Name "VMware.PowerCLI" | grep "No match" 2>&1 >/dev/null) && warnmsg "VMware.PowerCLI module is not installed for Powershell, Please see: https://docs.vmware.com/en/VMware-vSphere/7.0/com.vmware.esxi.install.doc/GUID-F02D0C2D-B226-4908-9E5C-2E783D41FE2D.html" && exit 1;
 	[[ "$FORMAT" == "vmdk"  ]] || { warnmsg "image must be \"vmdk\", convert with: \"qemu-img convert -O vmdk -o subformat=streamOptimized ${IMAGE_NAME} ${VMDK_NAME}.vmdk\""; exit 1; }
-
 }
 
 # Copy the vmdk to a folder on datastore root called "podvm"
 # Create a new VM using the vmdk as an attached disk
 # Convert the VM into a template
-# TODO: Add a delete option
 create_ps1script() {
     cat > ${TMPDIR}/config.ps1 << EOF
-param(\$Server, \$User, \$Password, \$Cluster, \$Datastore, \$VMDKPath, \$TemplateName, \$IgnoreSSL)
+param(\$Server, \$User, \$Password, \$Cluster, \$Datastore, \$VMDKPath, \$TemplateName, \$IgnoreSSL, \$Force)
 
 Write-Host "Connecting to \$Server"
 if (\$IgnoreSSL -eq 1) {
@@ -64,7 +64,20 @@ Connect-VIServer -Server \$Server -User \$User -Password \$Password
 \$datastore = Get-Datastore \$Datastore
 
 New-PSDrive -Location \$datastore -Name ds -PSProvider VimDatastore -Root "\"
-New-Item -Path ds:\podvm -ItemType Directory
+if (\$Force -eq 1) {
+   Write-Host "Attempting to remove \$TemplateName"
+   Remove-Template -Template \$TemplateName -Confirm:\$False
+   Remove-Item -Path ds:\\\$TemplateName -Recurse -Force
+}
+
+if (\$Force -eq 1) {
+   New-Item -Path ds:\podvm -Force -ItemType Directory
+} else {
+     New-Item -Path ds:\podvm -ItemType Directory
+     if (-not(\$?)) {
+         exit 1
+    }
+}
 
 Write-Host "Copying vmdk \$VMDKPath to \$Server/\$Datastore"
 Copy-DatastoreItem -Item \$VMDKPath -Destination ds:\podvm\podvm-base.vmdk
@@ -78,7 +91,7 @@ EOF
 }
 
 create_template() {
-    pwsh ${TMPDIR}/config.ps1 -Server $VCENTER_SERVER -User $VCENTER_USERNAME -Password $VCENTER_PASSWORD -Cluster $VCENTER_CLUSTER -VMDKPath $VMDK_PATH -TemplateName $TEMPLATE_NAME -IgnoreSSL $IGNORE_SSL -Datastore $VCENTER_DATASTORE
+    pwsh ${TMPDIR}/config.ps1 -Server $VCENTER_SERVER -User $VCENTER_USERNAME -Password $VCENTER_PASSWORD -Cluster $VCENTER_CLUSTER -VMDKPath $VMDK_PATH -TemplateName $TEMPLATE_NAME -IgnoreSSL $IGNORE_SSL -Datastore $VCENTER_DATASTORE  -Force $FORCE_INSTALL
 }
 
 clean_up() {
