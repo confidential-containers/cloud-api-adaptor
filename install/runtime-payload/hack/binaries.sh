be18439243f1cf1ea2104f0c3a517f1f7dc9ab20
Da Li Liu <liudali@cn.ibm.com>
2023-01-13 15:46:53 +0800

    operator: runtime payload multi-arch support - build out s390x containerd-shim-kata-v2 binary via cross go build - build s390x runtime payload docker image
    Fixes: #513
    
    Signed-off-by: Da Li Liu <liudali@cn.ibm.com>

--- install/runtime-payload/hack/binaries.sh
+++ install/runtime-payload/hack/binaries.sh
@@ -8,15 +8,22 @@ script_dir=$(dirname "$(readlink -f "$0")")
 bin_dir=${script_dir}/../bin
 kata_dir=${script_dir}/../../../../kata-containers
 
-function build_kata_runtime() {
-	pushd "${kata_dir}/src/runtime"
-        make
-	cp containerd-shim-kata-v2 ${bin_dir}/ 
+supported_go_arches=(
+	"x86_64"
+	"s390x"
+)
+
+function build_kata_shim_v2() {
+	pushd "${kata_dir}/src/runtime/cmd/containerd-shim-kata-v2"
+		for go_arch in ${supported_go_arches[@]}; do
+			env GOOS=linux GOARCH="${go_arch/x86_64/amd64}" CGO_ENABLED=0 go build -o containerd-shim-kata-v2-${go_arch}
+			cp containerd-shim-kata-v2-${go_arch} ${bin_dir}/
+		done
 	popd
 }
 function main() {
 	mkdir -p ${bin_dir}
-	build_kata_runtime
+	build_kata_shim_v2
 }
 
 main "$@"
