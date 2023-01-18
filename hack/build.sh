2adb716eec3f7f034983a07b196ea284137a1967
James Tumber <james.tumber@ibm.com>
2023-01-18 13:22:44 +0000

    operator: multiarch adaptor image
    - Build cross-compiled cloud-api-adaptor in builder step of Dockerfile.
    - Update build script to fully utilise buildx
    
    Fixes: #533
    
    Signed-off-by: James Tumber <james.tumber@ibm.com>

--- hack/build.sh
+++ hack/build.sh
@@ -6,58 +6,27 @@ set -o nounset
 
 script_dir=$(dirname "$(readlink -f "$0")")
 
-registry="${registry:-quay.io/confidential-containers/cloud-api-adaptor-${CLOUD_PROVIDER}}"
+registry="${registry:-quay.io/confidential-containers}"
+name="cloud-api-adaptor-${CLOUD_PROVIDER}"
+tag=$(date +%Y%m%d%H%M%s)
 
-supported_arches=(
-	"linux/amd64"
-)
-
-function setup_env_for_arch() {
-	case "$1" in
-		"linux/amd64")
-			kernel_arch="x86_64"
-			;;
-		(*) echo "$1 is not supported" && exit 1
-	esac
-}
+supported_arches="linux/amd64,linux/s390x"
 
 function build_caa_payload() {
 	pushd "${script_dir}/.."
 
-	tag=$(date +%Y%m%d%H%M%s)
-
-	for arch in ${supported_arches[@]}; do
-		setup_env_for_arch "${arch}"
-
-		echo "Building cloud-api-adaptor ${CLOUD_PROVIDER} image for ${arch}"
-		docker buildx build \
-			--build-arg ARCH="${kernel_arch}" \
-			--build-arg CLOUD_PROVIDER="${CLOUD_PROVIDER}" \
-			-f Dockerfile \
-			-t "${registry}:${kernel_arch}-${tag}" \
-			--platform="${arch}" \
-			--load \
-			.
-		docker push "${registry}:${kernel_arch}-${tag}"
-	done
-
-	docker manifest create \
-		${registry}:${tag} \
-		--amend ${registry}:x86_64-${tag}
-
-	docker manifest create \
-		${registry}:latest \
-		--amend ${registry}:x86_64-${tag}
-
-	docker manifest push ${registry}:${tag}
-	docker manifest push --purge ${registry}:latest
+	docker buildx build --platform "${supported_arches}" \
+		--build-arg CLOUD_PROVIDER="${CLOUD_PROVIDER}" \
+		-f Dockerfile \
+		-t "${registry}/${name}:${tag}" \
+		--push \
+		.
 
 	popd
 }
 
 function main() {
 	build_caa_payload
-	# TODO: kustomize with the tag
 }
 
 main "$@"
