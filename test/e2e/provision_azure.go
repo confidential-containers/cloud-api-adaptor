6e2b23d65cfc06bb176e6f9318d9af4bb002336d
Kartik Joshi <kartikjoshi@microsoft.com>
2023-02-22 03:47:11 +0530

    Azure: add azure provider in e2e test
    Fixes: #625
    
    Signed-off-by: Kartik Joshi <kartikjoshi@microsoft.com>

---
+++ test/e2e/provision_azure.go
@@ -0,0 +1,44 @@
+//go:build azure
+
+// (C) Copyright Confidential Containers Contributors
+// SPDX-License-Identifier: Apache-2.0
+
+package e2e
+
+import (
+	"context"
+	"sigs.k8s.io/e2e-framework/pkg/envconf"
+)
+
+// AzureCloudProvisioner implements the CloudProvision interface for ibmcloud.
+type AzureCloudProvisioner struct {
+}
+
+func NewAzureCloudProvisioner(network string, storage string) (*AzureCloudProvisioner, error) {
+	return &AzureCloudProvisioner{}, nil
+}
+
+func (l *AzureCloudProvisioner) CreateCluster(ctx context.Context, cfg *envconf.Config) error {
+	return nil
+}
+
+func (l *AzureCloudProvisioner) CreateVPC(ctx context.Context, cfg *envconf.Config) error {
+	return nil
+}
+
+func (l *AzureCloudProvisioner) DeleteCluster(ctx context.Context, cfg *envconf.Config) error {
+	return nil
+}
+
+func (l *AzureCloudProvisioner) DeleteVPC(ctx context.Context, cfg *envconf.Config) error {
+	return nil
+}
+
+func (l *AzureCloudProvisioner) UploadPodvm(imagePath string, ctx context.Context, cfg *envconf.Config) error {
+	return nil
+}
+
+//nolint:typecheck
+func GetCloudProvisioner() (CloudProvision, error) {
+	return NewAzureCloudProvisioner("default", "default")
+}
