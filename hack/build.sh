412c93e8b7081ac5083a86c99f458d17d1eb3b9e
Pradipta Banerjee <pradipta.banerjee@gmail.com>
2023-02-27 15:22:59 +0000

    gh-action: build dev and release images for every PR merge
    The images are tagged with the commit id, eg dev-f429523293643f29782fab9f7ad79de178f3dedc
    or f429523293643f29782fab9f7ad79de178f3dedc (for release builds).
    Further latest tag is only for dev images.
    
    Fixes: #637
    
    Signed-off-by: Pradipta Banerjee <pradipta.banerjee@gmail.com>

--- hack/build.sh
+++ hack/build.sh
@@ -8,21 +8,26 @@ script_dir=$(dirname "$(readlink -f "$0")")
 
 registry="${registry:-quay.io/confidential-containers}"
 name="cloud-api-adaptor"
-tag=$(date +%Y%m%d%H%M%s)
+commit_id=${1:-$(git rev-parse HEAD)}
 
 supported_arches=${ARCHES:-"linux/amd64"}
 
 function build_caa_payload() {
 	pushd "${script_dir}/.."
 
+	local tag_string="-t ${registry}/${name}:latest -t ${registry}/${name}:dev-${commit_id}"
+
+	if [[ "$RELEASE_BUILD" == "true" ]]; then
+		tag_string="-t ${registry}/${name}:${commit_id}"
+	fi
+
+
 	docker buildx build --platform "${supported_arches}" \
 		--build-arg RELEASE_BUILD="${RELEASE_BUILD}" \
 		-f Dockerfile \
-		-t "${registry}/${name}:${tag}" \
-		-t "${registry}/${name}:latest" \
+		${tag_string} \
 		--push \
 		.
-
 	popd
 }
 
