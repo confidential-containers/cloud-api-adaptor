6feb88ad9ebb902b528dc5032a409c66b2f1dc85
Amulyam24 <amulmek1@in.ibm.com>
2023-02-28 14:40:31 +0530

    peerpod: update go version to 1.19
    Bumps golang version from 1.18 to 1.19
    
    Fixes: #639
    
    Signed-off-by: Amulyam24 <amulmek1@in.ibm.com>

--- peer-pod-controller/api/v1alpha1/groupversion_info.go
+++ peer-pod-controller/api/v1alpha1/groupversion_info.go
@@ -15,8 +15,8 @@ limitations under the License.
 */
 
 // Package v1alpha1 contains API Schema definitions for the  v1alpha1 API group
-//+kubebuilder:object:generate=true
-//+groupName=confidentialcontainers.org
+// +kubebuilder:object:generate=true
+// +groupName=confidentialcontainers.org
 package v1alpha1
 
 import (
