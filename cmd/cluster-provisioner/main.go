23e2d16cdddb4065feb07ef23a42b568cb1d7a60
Qi Feng Huo <huoqif@cn.ibm.com>
2023-03-17 09:32:49 +0800

    ibmcloud: add e2e test overlay support Fixes: #712
    Signed-off-by: Qi Feng Huo <huoqif@cn.ibm.com>

--- cmd/cluster-provisioner/main.go
+++ cmd/cluster-provisioner/main.go
@@ -10,6 +10,7 @@ import (
 	"os"
 
 	pv "github.com/confidential-containers/cloud-api-adaptor/test/provisioner"
+	"sigs.k8s.io/e2e-framework/pkg/envconf"
 )
 
 // export LOG_LEVEL="trace|debug"
@@ -17,10 +18,14 @@ import (
 // export TEST_E2E_PROVISION_FILE="/root/provision_ibmcloud.properties"
 // export TEST_E2E_PODVM_IMAGE="/root/e2e-test-image-amd64-20230308.qcow2
 // export TEST_E2E_PROVISION="yes"
-// ./cluster-provisioner -action=provision | deprovision | uploadimage
+// cd test/e2e
+// ../../cluster-provisioner -action=provision | deprovision | uploadimage
+// TODO revise provisioner to enable run cluster-provisioner in any folder.
 func main() {
 	cloudProvider := os.Getenv("CLOUD_PROVIDER")
 	provisionPropsFile := os.Getenv("TEST_E2E_PROVISION_FILE")
+	podvmImage := os.Getenv("TEST_E2E_PODVM_IMAGE")
+	cfg := envconf.New()
 
 	provisioner, err := pv.GetCloudProvisioner(cloudProvider, provisionPropsFile)
 	if err != nil {
@@ -32,37 +37,62 @@ func main() {
 	flag.Parse()
 
 	if *action == "provision" {
-		if err := provisioner.CreateVPC(context.TODO(), nil); err != nil {
+		fmt.Println("Creating VPC...")
+		if err := provisioner.CreateVPC(context.TODO(), cfg); err != nil {
 			fmt.Println(err)
 			os.Exit(1)
 		}
 
-		if err := provisioner.CreateCluster(context.TODO(), nil); err != nil {
+		fmt.Println("Creating Cluster...")
+		if err := provisioner.CreateCluster(context.TODO(), cfg); err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+
+		if podvmImage != "" {
+			fmt.Println("Uploading PodVM Image...")
+			if _, err := os.Stat(podvmImage); os.IsNotExist(err) {
+				fmt.Println(err)
+				os.Exit(1)
+			}
+			if err := provisioner.UploadPodvm(podvmImage, context.TODO(), cfg); err != nil {
+				fmt.Println(err)
+				os.Exit(1)
+			}
+		}
+
+		cloudAPIAdaptor, err := pv.NewCloudAPIAdaptor(cloudProvider)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+		if err := cloudAPIAdaptor.Deploy(context.TODO(), cfg, provisioner.GetProperties(context.TODO(), cfg)); err != nil {
 			fmt.Println(err)
 			os.Exit(1)
 		}
 	}
 
 	if *action == "deprovision" {
-		if err := provisioner.DeleteCluster(context.TODO(), nil); err != nil {
+		fmt.Println("Deleting Cluster...")
+		if err := provisioner.DeleteCluster(context.TODO(), cfg); err != nil {
 			fmt.Println(err)
 			os.Exit(1)
 		}
 
-		if err := provisioner.DeleteVPC(context.TODO(), nil); err != nil {
+		fmt.Println("Deleting VPC...")
+		if err := provisioner.DeleteVPC(context.TODO(), cfg); err != nil {
 			fmt.Println(err)
 			os.Exit(1)
 		}
-
 	}
 
 	if *action == "uploadimage" {
-		podvmImage := os.Getenv("TEST_E2E_PODVM_IMAGE")
+		fmt.Println("Uploading PodVM Image...")
 		if _, err := os.Stat(podvmImage); os.IsNotExist(err) {
 			fmt.Println(err)
 			os.Exit(1)
 		}
-		if err := provisioner.UploadPodvm(podvmImage, context.TODO(), nil); err != nil {
+		if err := provisioner.UploadPodvm(podvmImage, context.TODO(), cfg); err != nil {
 			fmt.Println(err)
 			os.Exit(1)
 		}
