62fe27827ce26edff09c4163842175a2bc63db91
Yohei Ueda <yohei@jp.ibm.com>
2023-04-07 15:33:55 +0900

    aws: Process flags in in the provider code
    This patch moves the command line argument processing into
    the aws package.
    
    Signed-off-by: Yohei Ueda <yohei@jp.ibm.com>

---
+++ pkg/adaptor/cloud/aws/manager.go
@@ -0,0 +1,40 @@
+// (C) Copyright Confidential Containers Contributors
+// SPDX-License-Identifier: Apache-2.0
+
+package aws
+
+import (
+	"flag"
+
+	"github.com/confidential-containers/cloud-api-adaptor/pkg/adaptor/cloud"
+)
+
+var awscfg Config
+
+type Manager struct{}
+
+func (_ *Manager) ParseCmd(flags *flag.FlagSet) {
+
+	flags.StringVar(&awscfg.AccessKeyId, "aws-access-key-id", "", "Access Key ID, defaults to `AWS_ACCESS_KEY_ID`")
+	flags.StringVar(&awscfg.SecretKey, "aws-secret-key", "", "Secret Key, defaults to `AWS_SECRET_ACCESS_KEY`")
+	flags.StringVar(&awscfg.Region, "aws-region", "", "Region")
+	flags.StringVar(&awscfg.LoginProfile, "aws-profile", "test", "AWS Login Profile")
+	flags.StringVar(&awscfg.LaunchTemplateName, "aws-lt-name", "kata", "AWS Launch Template Name")
+	flags.BoolVar(&awscfg.UseLaunchTemplate, "use-lt", false, "Use EC2 Launch Template for the Pod VMs")
+	flags.StringVar(&awscfg.ImageId, "imageid", "", "Pod VM ami id")
+	flags.StringVar(&awscfg.InstanceType, "instance-type", "t3.small", "Pod VM instance type")
+	flags.Var(&awscfg.SecurityGroupIds, "securitygroupids", "Security Group Ids to be used for the Pod VM, comma separated")
+	flags.StringVar(&awscfg.KeyName, "keyname", "", "SSH Keypair name to be used with the Pod VM")
+	flags.StringVar(&awscfg.SubnetId, "subnetid", "", "Subnet ID to be used for the Pod VMs")
+
+}
+
+func (_ *Manager) LoadEnv() {
+	cloud.DefaultToEnv(&awscfg.AccessKeyId, "AWS_ACCESS_KEY_ID", "")
+	cloud.DefaultToEnv(&awscfg.SecretKey, "AWS_SECRET_ACCESS_KEY", "")
+
+}
+
+func (_ *Manager) NewProvider() (cloud.Provider, error) {
+	return NewProvider(&awscfg)
+}
--- pkg/adaptor/cloud/cloudmgr/aws.go
+++ pkg/adaptor/cloud/cloudmgr/aws.go
@@ -6,42 +6,9 @@
 package cloudmgr
 
 import (
-	"flag"
-
-	"github.com/confidential-containers/cloud-api-adaptor/pkg/adaptor/cloud"
 	"github.com/confidential-containers/cloud-api-adaptor/pkg/adaptor/cloud/aws"
 )
 
 func init() {
-	cloudTable["aws"] = &awsMgr{}
-}
-
-var awscfg aws.Config
-
-type awsMgr struct{}
-
-func (_ *awsMgr) ParseCmd(flags *flag.FlagSet) {
-
-	flags.StringVar(&awscfg.AccessKeyId, "aws-access-key-id", "", "Access Key ID, defaults to `AWS_ACCESS_KEY_ID`")
-	flags.StringVar(&awscfg.SecretKey, "aws-secret-key", "", "Secret Key, defaults to `AWS_SECRET_ACCESS_KEY`")
-	flags.StringVar(&awscfg.Region, "aws-region", "", "Region")
-	flags.StringVar(&awscfg.LoginProfile, "aws-profile", "test", "AWS Login Profile")
-	flags.StringVar(&awscfg.LaunchTemplateName, "aws-lt-name", "kata", "AWS Launch Template Name")
-	flags.BoolVar(&awscfg.UseLaunchTemplate, "use-lt", false, "Use EC2 Launch Template for the Pod VMs")
-	flags.StringVar(&awscfg.ImageId, "imageid", "", "Pod VM ami id")
-	flags.StringVar(&awscfg.InstanceType, "instance-type", "t3.small", "Pod VM instance type")
-	flags.Var(&awscfg.SecurityGroupIds, "securitygroupids", "Security Group Ids to be used for the Pod VM, comma separated")
-	flags.StringVar(&awscfg.KeyName, "keyname", "", "SSH Keypair name to be used with the Pod VM")
-	flags.StringVar(&awscfg.SubnetId, "subnetid", "", "Subnet ID to be used for the Pod VMs")
-
-}
-
-func (_ *awsMgr) LoadEnv() {
-	defaultToEnv(&awscfg.AccessKeyId, "AWS_ACCESS_KEY_ID")
-	defaultToEnv(&awscfg.SecretKey, "AWS_SECRET_ACCESS_KEY")
-
-}
-
-func (_ *awsMgr) NewProvider() (cloud.Provider, error) {
-	return aws.NewProvider(&awscfg)
+	cloudTable["aws"] = &aws.Manager{}
 }
