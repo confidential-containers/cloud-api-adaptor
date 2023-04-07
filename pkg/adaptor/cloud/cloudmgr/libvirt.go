e3ed77b73c44320df706261f6aefc6a397bc0602
Yohei Ueda <yohei@jp.ibm.com>
2023-04-07 15:21:57 +0900

    libvirt: Process flags in in the provider code
    This patch moves the command line argument processing into
    the libvirt package.
    
    Signed-off-by: Yohei Ueda <yohei@jp.ibm.com>

--- pkg/adaptor/cloud/cloudmgr/libvirt.go
+++ pkg/adaptor/cloud/cloudmgr/libvirt.go
@@ -6,35 +6,9 @@
 package cloudmgr
 
 import (
-	"flag"
-
-	"github.com/confidential-containers/cloud-api-adaptor/pkg/adaptor/cloud"
 	"github.com/confidential-containers/cloud-api-adaptor/pkg/adaptor/cloud/libvirt"
 )
 
 func init() {
-	cloudTable["libvirt"] = &libvirtMgr{}
-}
-
-var libvirtcfg libvirt.Config
-
-type libvirtMgr struct{}
-
-func (_ *libvirtMgr) ParseCmd(flags *flag.FlagSet) {
-
-	flags.StringVar(&libvirtcfg.URI, "uri", "qemu:///system", "libvirt URI")
-	flags.StringVar(&libvirtcfg.PoolName, "pool-name", "default", "libvirt storage pool")
-	flags.StringVar(&libvirtcfg.NetworkName, "network-name", "default", "libvirt network pool")
-	flags.StringVar(&libvirtcfg.DataDir, "data-dir", "/var/lib/libvirt/images", "libvirt storage dir")
-
-}
-
-func (_ *libvirtMgr) LoadEnv() {
-	defaultToEnv(&libvirtcfg.URI, "LIBVIRT_URI", "qemu:///system")
-	defaultToEnv(&libvirtcfg.PoolName, "LIBVIRT_POOL", "default")
-	defaultToEnv(&libvirtcfg.NetworkName, "LIBVIRT_NET", "default")
-}
-
-func (_ *libvirtMgr) NewProvider() (cloud.Provider, error) {
-	return libvirt.NewProvider(&libvirtcfg)
+	cloudTable["libvirt"] = &libvirt.Manager{}
 }
---
+++ pkg/adaptor/cloud/libvirt/manager.go
@@ -0,0 +1,42 @@
+//go:build cgo
+
+// (C) Copyright Confidential Containers Contributors
+// SPDX-License-Identifier: Apache-2.0
+
+package libvirt
+
+import (
+	"flag"
+
+	"github.com/confidential-containers/cloud-api-adaptor/pkg/adaptor/cloud"
+)
+
+var libvirtcfg Config
+
+type Manager struct{}
+
+const (
+	defaultURI         = "qemu:///system"
+	defaultPoolName    = "default"
+	defaultNetworkName = "default"
+	defaultDataDir     = "/var/lib/libvirt/images"
+)
+
+func (_ *Manager) ParseCmd(flags *flag.FlagSet) {
+
+	flags.StringVar(&libvirtcfg.URI, "uri", defaultURI, "libvirt URI")
+	flags.StringVar(&libvirtcfg.PoolName, "pool-name", defaultPoolName, "libvirt storage pool")
+	flags.StringVar(&libvirtcfg.NetworkName, "network-name", defaultNetworkName, "libvirt network pool")
+	flags.StringVar(&libvirtcfg.DataDir, "data-dir", defaultDataDir, "libvirt storage dir")
+
+}
+
+func (_ *Manager) LoadEnv() {
+	cloud.DefaultToEnv(&libvirtcfg.URI, "LIBVIRT_URI", defaultURI)
+	cloud.DefaultToEnv(&libvirtcfg.PoolName, "LIBVIRT_POOL", defaultPoolName)
+	cloud.DefaultToEnv(&libvirtcfg.NetworkName, "LIBVIRT_NET", defaultNetworkName)
+}
+
+func (_ *Manager) NewProvider() (cloud.Provider, error) {
+	return NewProvider(&libvirtcfg)
+}
