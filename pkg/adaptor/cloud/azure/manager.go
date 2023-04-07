904ba1de2500d8fe48fda3d344752ea87984a339
Yohei Ueda <yohei@jp.ibm.com>
2023-04-07 15:39:40 +0900

    azure: Process flags in in the provider code
    This patch moves the command line argument processing into
    the azure package.
    
    Signed-off-by: Yohei Ueda <yohei@jp.ibm.com>

---
+++ pkg/adaptor/cloud/azure/manager.go
@@ -0,0 +1,40 @@
+// (C) Copyright Confidential Containers Contributors
+// SPDX-License-Identifier: Apache-2.0
+
+package azure
+
+import (
+	"flag"
+
+	"github.com/confidential-containers/cloud-api-adaptor/pkg/adaptor/cloud"
+)
+
+var azurecfg Config
+
+type Manager struct{}
+
+func (_ *Manager) ParseCmd(flags *flag.FlagSet) {
+	flags.StringVar(&azurecfg.ClientId, "clientid", "", "Client Id, defaults to `AZURE_CLIENT_ID`")
+	flags.StringVar(&azurecfg.ClientSecret, "secret", "", "Client Secret, defaults to `AZURE_CLIENT_SECRET`")
+	flags.StringVar(&azurecfg.TenantId, "tenantid", "", "Tenant Id, defaults to `AZURE_TENANT_ID`")
+	flags.StringVar(&azurecfg.ResourceGroupName, "resourcegroup", "", "Resource Group")
+	flags.StringVar(&azurecfg.Zone, "zone", "", "Zone")
+	flags.StringVar(&azurecfg.Region, "region", "", "Region")
+	flags.StringVar(&azurecfg.SubnetId, "subnetid", "", "Network Subnet Id")
+	flags.StringVar(&azurecfg.SecurityGroupId, "securitygroupid", "", "Security Group Id")
+	flags.StringVar(&azurecfg.Size, "instance-size", "", "Instance size")
+	flags.StringVar(&azurecfg.ImageId, "imageid", "", "Image Id")
+	flags.StringVar(&azurecfg.SubscriptionId, "subscriptionid", "", "Subscription ID")
+	flags.StringVar(&azurecfg.SSHKeyPath, "ssh-key-path", "$HOME/.ssh/id_rsa.pub", "Path to SSH public key")
+}
+
+func (_ *Manager) LoadEnv() {
+	cloud.DefaultToEnv(&azurecfg.ClientId, "AZURE_CLIENT_ID", "")
+	cloud.DefaultToEnv(&azurecfg.ClientSecret, "AZURE_CLIENT_SECRET", "")
+	cloud.DefaultToEnv(&azurecfg.TenantId, "AZURE_TENANT_ID", "")
+
+}
+
+func (_ *Manager) NewProvider() (cloud.Provider, error) {
+	return NewProvider(&azurecfg)
+}
