bdb35b9ed9cd80e1183d2c8302fd0718a0f925ca
Pradipta Banerjee <pradipta.banerjee@gmail.com>
2023-04-21 22:07:47 +0530

    azure: Update copy-files.sh
    Update copy-files.sh to be in sync with other providers
    
    Signed-off-by: Pradipta Banerjee <pradipta.banerjee@gmail.com>

--- azure/image/copy-files.sh
+++ azure/image/copy-files.sh
@@ -1,3 +1,6 @@
+# copy-files.sh is used to copy required files into
+# the correct location on the podvm image
+
 sudo mkdir -p /etc/containers
 sudo cp /tmp/files/etc/agent-config.toml /etc/agent-config.toml
 sudo cp -a /tmp/files/etc/containers/* /etc/containers/
@@ -6,13 +9,18 @@ if [ -e /tmp/files/etc/aa-offline_fs_kbc-resources.json ]; then
 	sudo cp /tmp/files/etc/aa-offline_fs_kbc-resources.json /etc/aa-offline_fs_kbc-resources.json
 fi
 
+if [ -e /tmp/files/etc/certificates/tls.crt ] && [ -e /tmp/files/etc/certificates/tls.key ]; then
+        sudo mkdir -p /etc/certificates
+	sudo cp -a /tmp/files/etc/certificates/tls.crt /etc/certificates/
+	sudo cp -a /tmp/files/etc/certificates/tls.key /etc/certificates/
+fi
+
+# If self-signed certificates, then ca certificate needs to be provided
+if [ -e /tmp/files/etc/certificates/ca.crt ]; then
+	sudo cp -a /tmp/files/etc/certificates/ca.crt /etc/certificates/
+fi
 
 sudo mkdir -p /usr/local/bin
 sudo cp -a /tmp/files/usr/* /usr/
 
 sudo cp -a /tmp/files/pause_bundle /
-
-if [ -e /tmp/files/auth.json ]; then
-       sudo mkdir -p /root/.config/containers/
-       sudo cp -a /tmp/files/auth.json /root/.config/containers/auth.json
-fi
