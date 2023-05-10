07387d1a278cc5a14a74d107628c0e0a47e09c22
Matthew Arnold <mattarno@uk.ibm.com>
2023-05-10 13:21:30 +0100

    test/e2e: Refactor kata e2e tests
    Refactor provider-common e2e tests, using the options
    and builder patterns, so we have common
    methods for creating pod objects that all the test
    cases use.
    
    Fixes: #918
    
    Signed-off-by: Matthew Arnold <mattarno@uk.ibm.com>

--- test/e2e/common.go
+++ test/e2e/common.go
@@ -7,63 +7,72 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-// newPod returns a new Pod object.
-func newPod(namespace string, name string, containerName string, runtimeclass string) *corev1.Pod {
-	return &corev1.Pod{
-		ObjectMeta: metav1.ObjectMeta{Name: name, Namespace: namespace},
-		Spec: corev1.PodSpec{
-			Containers:       []corev1.Container{{Name: containerName, Image: "nginx"}},
-			DNSPolicy:        "ClusterFirst",
-			RestartPolicy:    "Never",
-			RuntimeClassName: &runtimeclass,
-		},
+type podOption func(*corev1.Pod)
+
+func withRestartPolicy(restartPolicy corev1.RestartPolicy) podOption {
+	return func(p *corev1.Pod) {
+		p.Spec.RestartPolicy = restartPolicy
 	}
 }
 
-// newPod returns a new Pod object.
-func newBusyboxPod(namespace string, name string, containerName string, runtimeclass string) *corev1.Pod {
-	return &corev1.Pod{
-		ObjectMeta: metav1.ObjectMeta{Name: name, Namespace: namespace},
-		Spec: corev1.PodSpec{
-			Containers:       []corev1.Container{{Name: containerName, Image: "quay.io/prometheus/busybox:latest", Command: []string{"/bin/sh", "-c", "sleep 3600"}}},
-			DNSPolicy:        "ClusterFirst",
-			RuntimeClassName: &runtimeclass,
-			RestartPolicy:    corev1.RestartPolicyAlways,
-		},
+func withCommand(command []string) podOption {
+	return func(p *corev1.Pod) {
+		p.Spec.Containers[0].Command = command
 	}
 }
 
-func newPodWithConfigMap(namespace string, name string, containerName string, runtimeclass string, configmapname string) *corev1.Pod {
-	return &corev1.Pod{
-		ObjectMeta: metav1.ObjectMeta{Name: name, Namespace: namespace},
-		Spec: corev1.PodSpec{
-			Containers:       []corev1.Container{{Name: containerName, Image: "nginx", VolumeMounts: []corev1.VolumeMount{{Name: "config-volume", MountPath: "/etc/config"}}}},
-			DNSPolicy:        "ClusterFirst",
-			RestartPolicy:    "Never",
-			RuntimeClassName: &runtimeclass,
-			Volumes:          []corev1.Volume{{Name: "config-volume", VolumeSource: corev1.VolumeSource{ConfigMap: &corev1.ConfigMapVolumeSource{LocalObjectReference: corev1.LocalObjectReference{Name: configmapname}}}}},
-		},
+func withConfigMapBinding(mountPath string, configMapName string) podOption {
+	return func(p *corev1.Pod) {
+		p.Spec.Containers[0].VolumeMounts = append(p.Spec.Containers[0].VolumeMounts, corev1.VolumeMount{Name: "config-volume", MountPath: mountPath})
+		p.Spec.Volumes = append(p.Spec.Volumes, corev1.Volume{Name: "config-volume", VolumeSource: corev1.VolumeSource{ConfigMap: &corev1.ConfigMapVolumeSource{LocalObjectReference: corev1.LocalObjectReference{Name: configMapName}}}})
 	}
 }
-func newPodWithSecret(namespace string, name string, containerName string, runtimeclass string, secretname string) *corev1.Pod {
-	return &corev1.Pod{
-		ObjectMeta: metav1.ObjectMeta{Name: name, Namespace: namespace},
+
+func withSecretBinding(mountPath string, secretName string) podOption {
+	return func(p *corev1.Pod) {
+		p.Spec.Containers[0].VolumeMounts = append(p.Spec.Containers[0].VolumeMounts, corev1.VolumeMount{Name: "secret-volume", MountPath: mountPath})
+		p.Spec.Volumes = append(p.Spec.Volumes, corev1.Volume{Name: "secret-volume", VolumeSource: corev1.VolumeSource{Secret: &corev1.SecretVolumeSource{SecretName: secretName}}})
+	}
+}
+
+func newPod(namespace string, podName string, containerName string, imageName string, options ...podOption) *corev1.Pod {
+	runtimeClassName := "kata-remote"
+	pod := &corev1.Pod{
+		ObjectMeta: metav1.ObjectMeta{Name: podName, Namespace: namespace},
 		Spec: corev1.PodSpec{
-			Containers:       []corev1.Container{{Name: containerName, Image: "nginx", VolumeMounts: []corev1.VolumeMount{{Name: "secret-volume", MountPath: "/etc/secret"}}}},
-			DNSPolicy:        "ClusterFirst",
-			RestartPolicy:    "Never",
-			RuntimeClassName: &runtimeclass,
-			Volumes:          []corev1.Volume{{Name: "secret-volume", VolumeSource: corev1.VolumeSource{Secret: &corev1.SecretVolumeSource{SecretName: secretname}}}},
+			Containers:       []corev1.Container{{Name: containerName, Image: imageName}},
+			RuntimeClassName: &runtimeClassName,
 		},
 	}
+
+	for _, option := range options {
+		option(pod)
+	}
+
+	return pod
+}
+
+func newNginxPod(namespace string) *corev1.Pod {
+	return newPod(namespace, "nginx", "nginx", "nginx", withRestartPolicy(corev1.RestartPolicyNever))
+}
+
+func newBusyboxPod(namespace string) *corev1.Pod {
+	return newPod(namespace, "busybox-pod", "busybox", "quay.io/prometheus/busybox:latest", withCommand([]string{"/bin/sh", "-c", "sleep 3600"}))
+}
+
+func newNginxPodWithConfigMap(namespace string, configMapName string) *corev1.Pod {
+	return newPod(namespace, "nginx-configmap-pod", "nginx-configmap", "nginx", withRestartPolicy(corev1.RestartPolicyNever), withConfigMapBinding("/etc/config", configMapName))
+}
+
+func newNginxPodWithSecret(namespace string, secretName string) *corev1.Pod {
+	return newPod(namespace, "nginx-secret-pod", "nginx-secret", "nginx", withRestartPolicy(corev1.RestartPolicyNever), withSecretBinding("/etc/secret", secretName))
 }
 
 // newConfigMap returns a new config map object.
-func newConfigMap(namespace string, name string, configmapData map[string]string) *corev1.ConfigMap {
-
+func newConfigMap(namespace string, name string, configMapData map[string]string) *corev1.ConfigMap {
 	return &corev1.ConfigMap{
 		ObjectMeta: metav1.ObjectMeta{Name: name, Namespace: namespace},
-		Data:       configmapData,
+		Data:       configMapData,
 	}
 }
 
