93c8fc61012db929ec9feb172d192313cd50c427
Paul Meyer <49727155+katexochen@users.noreply.github.com>
2023-05-12 16:47:55 +0200

    shellcheck: fix redirects of stdout+stderr
    To redirect stdout+stderr, 2>&1 must be last (or use
    { cmd > file; } 2>&1 to clarify), see SC2069.
    
    Signed-off-by: Paul Meyer <49727155+katexochen@users.noreply.github.com>

--- podvm/qcow2/build-s390x-se-image.sh
+++ podvm/qcow2/build-s390x-se-image.sh
@@ -18,8 +18,8 @@ done
 [[ -z $host_keys ]] && echo "Didn't find host key files, please download host key files to 'files' folder " && exit 1
 echo "Installing jq"
 export DEBIAN_FRONTEND=noninteractive
-sudo apt-get update 2>&1 >/dev/null
-sudo apt-get install jq -y 2>&1 >/dev/null
+sudo apt-get update > /dev/null 2>&1
+sudo apt-get install jq -y > /dev/null 2>&1
 sudo apt-get remove unattended-upgrades -y
 sudo apt-get autoremove
 sudo apt-get clean
