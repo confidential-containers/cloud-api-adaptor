02dab52098dec385fa54670880520f7fe223587a
Pradipta Banerjee <pradipta.banerjee@gmail.com>
2023-06-22 18:07:49 +0530

    cloud: Add helper methods to help with instance selection
    Add few helper methods to help providers with selecting the
    best instance type from configured instance list
    
    Fixes: #321
    
    Signed-off-by: Pradipta Banerjee <pradipta.banerjee@gmail.com>

--- pkg/util/cloud.go
+++ pkg/util/cloud.go
@@ -106,3 +106,13 @@ func GetCPUAndMemoryFromAnnotation(annotations map[string]string) (int64, int64)
 	// Return vCPU and memory
 	return vcpuInt, memoryInt
 }
+
+// Method to check if a string exists in a slice
+func Contains(slice []string, s string) bool {
+	for _, item := range slice {
+		if item == s {
+			return true
+		}
+	}
+	return false
+}
