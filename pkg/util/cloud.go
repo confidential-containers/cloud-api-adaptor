b9d8a00ee90d187f6e19b5ad103b19c834f3cb1d
Pradipta Banerjee <pradipta.banerjee@gmail.com>
2023-06-22 17:54:45 +0530

    util: Add methods to retrieve instance type, vpcu, mem from annotations
    Retrieve the instance type (flavor), vcpus, mem from the pod
    annotations. This will be used by CAA to decide the right
    cloud instance type to be used
    
    Signed-off-by: Pradipta Banerjee <pradipta.banerjee@gmail.com>

--- pkg/util/cloud.go
+++ pkg/util/cloud.go
@@ -2,9 +2,11 @@ package util
 
 import (
 	"fmt"
+	"strconv"
 	"strings"
 
 	cri "github.com/containerd/containerd/pkg/cri/annotations"
+	hypannotations "github.com/kata-containers/kata-containers/src/runtime/virtcontainers/pkg/annotations"
 )
 
 const (
@@ -61,4 +63,46 @@ func GetPodName(annotations map[string]string) string {
 func GetPodNamespace(annotations map[string]string) string {
 
 	return annotations[cri.SandboxNamespace]
+}
+
+// Method to get instance type from annotation
+func GetInstanceTypeFromAnnotation(annotations map[string]string) string {
+	// The machine_type annotation in Kata refers to VM type
+	// For example machine_type for Kata/Qemu refers to pc, q35, microvm etc.
+	// We use the same annotation for Kata/remote to refer to cloud instance type (flavor)
+	return annotations[hypannotations.MachineType]
+}
+
+// Method to get vCPU and memory from annotations
+func GetCPUAndMemoryFromAnnotation(annotations map[string]string) (int64, int64) {
+
+	var vcpuInt, memoryInt int64
+	var err error
+
+	vcpu, ok := annotations[hypannotations.DefaultVCPUs]
+	if ok {
+		vcpuInt, err = strconv.ParseInt(vcpu, 10, 64)
+		if err != nil {
+			fmt.Printf("Error converting vcpu to int64. Defaulting to 0: %v\n", err)
+			vcpuInt = 0
+		}
+	} else {
+		vcpuInt = 0
+	}
+
+	memory, ok := annotations[hypannotations.DefaultMemory]
+	if ok {
+		// Use strconv.ParseInt to convert string to int64
+		memoryInt, err = strconv.ParseInt(memory, 10, 64)
+		if err != nil {
+			fmt.Printf("Error converting memory to int64. Defaulting to 0: %v\n", err)
+			memoryInt = 0
+		}
+
+	} else {
+		memoryInt = 0
+	}
+
+	// Return vCPU and memory
+	return vcpuInt, memoryInt
 }
