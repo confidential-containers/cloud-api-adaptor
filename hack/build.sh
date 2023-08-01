1838774c61da28dd406cfb7578c03cc1152c08a6
Wainer dos Santos Moschetta <wainersm@redhat.com>
2023-08-01 16:53:06 -0300

    hack/build.sh: allow to overwrite the image tags
    Currently the hack/build.sh builds the cloud-api-adaptor image in dev and
    release modes. The image is tagged as `dev-$sha1` and `latest` when in
    dev mode, and just `$sha1` for releases. However we may want to tag the
    image differently, for example, on releases context the release image
    should have the git tag (e.g. `v0.7.0`). This changed the script to
    optionally read from the `DEV_TAGS` and `RELEASE_TAGS` environment
    variables the tags that should be used.
    
    For instance, `make image RELEASE_BUILD=true RELEASE_TAGS=v0.7.0` will push to
    quay.io/confidential-containers/cloud-api-adaptor:v0.7.0 a release
    image.
    
    Signed-off-by: Wainer dos Santos Moschetta <wainersm@redhat.com>

--- hack/build.sh
+++ hack/build.sh
@@ -17,16 +17,34 @@ if [[ "$commit" = unknown ]]; then
 	[[ -n "$(git status --porcelain --untracked-files=no)" ]] && commit+='-dirty'
 fi
 
+dev_tags=${DEV_TAGS:-"latest,dev-${commit}"}
+release_tags=${RELEASE_TAGS:-"${commit}"}
+
 supported_arches=${ARCHES:-"linux/amd64"}
+
+# Get a list of comma-separated tags (e.g. latest,dev-5d0da3dc9764), return
+# the tag string (e.g "-t ${registry}/${name}:latest -t ${registry}/${name}:dev-5d0da3dc9764")
+#
+function get_tag_string() {
+	local tags="$1"
+	local tag_string=""
+
+	for tag in ${tags/,/ };do
+		tag_string+=" -t ${registry}/${name}:${tag}"
+	done
+
+	echo "$tag_string"
+}
 
 function build_caa_payload() {
 	pushd "${script_dir}/.."
 
-	local tag_string="-t ${registry}/${name}:latest -t ${registry}/${name}:dev-${commit}"
+	local tag_string
 	local build_type=dev
 
+	tag_string="$(get_tag_string "$dev_tags")"
 	if [[ "$release_build" == "true" ]]; then
-		tag_string="-t ${registry}/${name}:${commit}"
+		tag_string="$(get_tag_string "$release_tags")"
 		build_type=release
 	fi
 
