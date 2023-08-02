dcf00e246f46fc75f2ba9688e2faf08bfac476a9
Pradipta Banerjee <pradipta.banerjee@gmail.com>
2023-08-02 09:31:29 -0400

    cloud: introduce KeyValueFlag type for CLI
    This is a new type to handle k1=v1,k2=v2 types of values
    
    Fixes: #1280
    Signed-off-by: Pradipta Banerjee <pradipta.banerjee@gmail.com>

--- pkg/adaptor/cloud/types.go
+++ pkg/adaptor/cloud/types.go
@@ -5,7 +5,10 @@ package cloud
 
 import (
 	"context"
+	"errors"
+	"fmt"
 	"net/netip"
+	"strings"
 	"sync"
 
 	"github.com/confidential-containers/cloud-api-adaptor/pkg/adaptor/k8sops"
@@ -67,4 +70,38 @@ type sandbox struct {
 	instanceID   string
 	netNSPath    string
 	spec         InstanceTypeSpec
+}
+
+// keyValueFlag represents a flag of key-value pairs
+type KeyValueFlag map[string]string
+
+// String returns the string representation of the keyValueFlag
+func (k *KeyValueFlag) String() string {
+	var pairs []string
+	for key, value := range *k {
+		pairs = append(pairs, fmt.Sprintf("%s=%s", key, value))
+	}
+	return strings.Join(pairs, ", ")
+}
+
+// Set parses the input string and sets the keyValueFlag value
+func (k *KeyValueFlag) Set(value string) error {
+	// Check if keyValueFlag is initialized. If not initialize it
+	if *k == nil {
+		*k = make(KeyValueFlag, 0)
+	}
+	pairs := strings.Split(value, ",")
+	for _, pair := range pairs {
+		keyValue := strings.SplitN(pair, "=", 2)
+		if len(keyValue) != 2 {
+			return errors.New("invalid key-value pair: " + pair)
+		}
+		key := strings.TrimSpace(keyValue[0])
+		value := strings.TrimSpace(keyValue[1])
+		// Append the key, value to the map
+		(*k)[key] = value
+
+	}
+
+	return nil
 }
