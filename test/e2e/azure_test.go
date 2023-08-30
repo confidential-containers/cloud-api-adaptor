cc34a129e28fba08533481c749c82f7d87f0d017
Magnus Kulke <magnuskulke@microsoft.com>
2023-08-30 12:33:15 +0200

    test: misc enhancements to the azure e2e test code
    - Remove option to pass secrets to test run
    
      since leakage of secrets in the CI is a risk and
      federated credentials work well on github actions we
      remove the option to pass a CLIENT_SECRET to a test
      run. Local runs can keep using cli auth.
    
    - Parallize the tests
    
      it will speed up a test run and is a valid test path.
      there are no dependencies between the test cases, so
      flakes due to this would imply a bug we want to be
      aware of.
    
    - There is a misnomer in the ssh key parameter.
    
    - There is no need to query the API for the rg name in the
      cloud asserter, since we have the name in all cases.
    
    Signed-off-by: Magnus Kulke <magnuskulke@microsoft.com>

--- test/e2e/azure_test.go
+++ test/e2e/azure_test.go
@@ -10,42 +10,33 @@ import (
 	"strings"
 	"testing"
 
-	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/resources/armresources"
 	pv "github.com/confidential-containers/cloud-api-adaptor/test/provisioner"
 	log "github.com/sirupsen/logrus"
 )
 
+// AzureCloudAssert implements the CloudAssert interface for azure.
+type AzureCloudAssert struct{}
+
+var assert = &AzureCloudAssert{}
+
 func TestDeletePodAzure(t *testing.T) {
-	assert := AzureCloudAssert{
-		group: pv.AzureProps.ResourceGroup,
-	}
+	t.Parallel()
 	doTestDeleteSimplePod(t, assert)
 }
 
 func TestCreateSimplePodAzure(t *testing.T) {
-	assert := AzureCloudAssert{
-		group: pv.AzureProps.ResourceGroup,
-	}
+	t.Parallel()
 	doTestCreateSimplePod(t, assert)
 }
 
 func TestCreatePodWithConfigMapAzure(t *testing.T) {
-	assert := AzureCloudAssert{
-		group: pv.AzureProps.ResourceGroup,
-	}
+	t.Parallel()
 	doTestCreatePodWithConfigMap(t, assert)
 }
 
 func TestCreatePodWithSecretAzure(t *testing.T) {
-	assert := AzureCloudAssert{
-		group: pv.AzureProps.ResourceGroup,
-	}
+	t.Parallel()
 	doTestCreatePodWithSecret(t, assert)
-}
-
-// AzureCloudAssert implements the CloudAssert interface for azure.
-type AzureCloudAssert struct {
-	group *armresources.ResourceGroup
 }
 
 func checkVMExistence(resourceGroupName, prefixName string) bool {
@@ -72,10 +63,11 @@ func checkVMExistence(resourceGroupName, prefixName string) bool {
 
 func (c AzureCloudAssert) HasPodVM(t *testing.T, id string) {
 	pod_vm_prefix := "podvm-" + id
-	if checkVMExistence(*c.group.Name, pod_vm_prefix) {
+	rg := pv.AzureProps.ResourceGroupName
+	if checkVMExistence(rg, pod_vm_prefix) {
 		log.Infof("VM found in resource group")
 	} else {
-		log.Infof("Virtual machine %s not found in resource group ", id)
+		log.Infof("Virtual machine %s not found in resource group %s", id, rg)
 		t.Error("PodVM was not created")
 	}
 }
