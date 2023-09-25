910a4b18bfd3f9bf60bc573fb97958e0cd2c6533
Amulyam24 <amulmek1@in.ibm.com>
2023-09-25 18:43:13 +0530

    podvm: fix attestation-agent build on ppc64le
    Fix the attestation-agent build by installing perl packages as one of the dependencies.
    
    Fixes: #1468
    
    Signed-off-by: Amulyam24 <amulmek1@in.ibm.com>

--- ibmcloud-powervs/image/prereq.sh
+++ ibmcloud-powervs/image/prereq.sh
@@ -5,7 +5,7 @@ RUST_VERSION="1.72.0"
 SKOPEO_VERSION="1.5.0"
 
 # Install dependencies
-yum install -y curl protobuf-compiler libseccomp-devel openssl openssl-devel skopeo-${SKOPEO_VERSION}
+yum install -y curl protobuf-compiler libseccomp-devel openssl openssl-devel perl skopeo-${SKOPEO_VERSION}
 
 # Install Golang
 curl https://dl.google.com/go/go${GO_VERSION}.linux-ppc64le.tar.gz -o go${GO_VERSION}.linux-ppc64le.tar.gz && \
