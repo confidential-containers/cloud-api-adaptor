54cb09d2aba3655a7b20ad6a11e7b1868f4cb30d
Paul Meyer <49727155+katexochen@users.noreply.github.com>
2023-10-24 07:35:13 +0000

    podvm-mkosi: print info and PCRs to serial console
    Working with the new images, two information about the image are of
    fundamental interest both for devs as for users: the vTPM PCR values
    of the image and whether the image is a debug image or intended for
    production use. This changes adds printing capabilities for those
    information.
    
    As production images don't allow access via SSH or serial console, we
    print everything to the serial console after boot. For dev convenience,
    the debug image will also print the image info on SSH login.
    
    Signed-off-by: Paul Meyer <49727155+katexochen@users.noreply.github.com>

---
+++ podvm-mkosi/mkosi.skeleton/etc/profile.d/10-alias.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+alias neofetch='neofetch \
+      --config /etc/neofetch/config.conf \
+      --ascii /etc/neofetch/coco.ascii \
+      --colors 9 9 9 9 9 12 \
+      --ascii_colors 24 9'
