692fde2d88b6092a402a1ca9fb5f92b993d2201e
James Tumber <james.tumber@ibm.com>
2023-11-16 12:15:42 +0000

    provider: move Instance related types to provider module
    Extract out the generic instance related types
    
    Signed-off-by: James Tumber <james.tumber@ibm.com>

--- pkg/adaptor/cloud/types.go
+++ pkg/adaptor/cloud/types.go
@@ -5,32 +5,16 @@ package cloud
 
 import (
 	"context"
-	"errors"
-	"fmt"
-	"net/netip"
-	"strings"
 	"sync"
 
 	"github.com/confidential-containers/cloud-api-adaptor/pkg/adaptor/k8sops"
 	"github.com/confidential-containers/cloud-api-adaptor/pkg/adaptor/proxy"
 	"github.com/confidential-containers/cloud-api-adaptor/pkg/podnetwork"
 	"github.com/confidential-containers/cloud-api-adaptor/pkg/podnetwork/tunneler"
-	"github.com/confidential-containers/cloud-api-adaptor/pkg/util/cloudinit"
+	"github.com/confidential-containers/cloud-api-adaptor/provider"
+	"github.com/confidential-containers/cloud-api-adaptor/provider/util/cloudinit"
 	pb "github.com/kata-containers/kata-containers/src/runtime/protocols/hypervisor"
 )
-
-type Provider interface {
-	CreateInstance(ctx context.Context, podName, sandboxID string, cloudConfig cloudinit.CloudConfigGenerator, spec InstanceTypeSpec) (instance *Instance, err error)
-	DeleteInstance(ctx context.Context, instanceID string) error
-	Teardown() error
-	ConfigVerifier() error
-}
-
-type Instance struct {
-	ID   string
-	Name string
-	IPs  []netip.Addr
-}
 
 type Service interface {
 	pb.HypervisorService
@@ -40,7 +24,7 @@ type Service interface {
 }
 
 type cloudService struct {
-	provider     Provider
+	provider     provider.Provider
 	proxyFactory proxy.Factory
 	workerNode   podnetwork.WorkerNode
 	sandboxes    map[sandboxID]*sandbox
@@ -50,14 +34,6 @@ type cloudService struct {
 	mutex        sync.Mutex
 	ppService    *k8sops.PeerPodService
 	aaKBCParams  string
-}
-
-type InstanceTypeSpec struct {
-	InstanceType string
-	VCPUs        int64
-	Memory       int64
-	Arch         string
-	GPUs         int64
 }
 
 type sandboxID string
@@ -72,39 +48,5 @@ type sandbox struct {
 	instanceName string
 	instanceID   string
 	netNSPath    string
-	spec         InstanceTypeSpec
-}
-
-// keyValueFlag represents a flag of key-value pairs
-type KeyValueFlag map[string]string
-
-// String returns the string representation of the keyValueFlag
-func (k *KeyValueFlag) String() string {
-	var pairs []string
-	for key, value := range *k {
-		pairs = append(pairs, fmt.Sprintf("%s=%s", key, value))
-	}
-	return strings.Join(pairs, ", ")
-}
-
-// Set parses the input string and sets the keyValueFlag value
-func (k *KeyValueFlag) Set(value string) error {
-	// Check if keyValueFlag is initialized. If not initialize it
-	if *k == nil {
-		*k = make(KeyValueFlag, 0)
-	}
-	pairs := strings.Split(value, ",")
-	for _, pair := range pairs {
-		keyValue := strings.SplitN(pair, "=", 2)
-		if len(keyValue) != 2 {
-			return errors.New("invalid key-value pair: " + pair)
-		}
-		key := strings.TrimSpace(keyValue[0])
-		value := strings.TrimSpace(keyValue[1])
-		// Append the key, value to the map
-		(*k)[key] = value
-
-	}
-
-	return nil
+	spec         provider.InstanceTypeSpec
 }
