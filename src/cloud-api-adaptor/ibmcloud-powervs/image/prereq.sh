f3c941d96c9fa76431cace58ec422fe1529d7dbe
Vaibhav-Nazare <87978158+Vaibhav-Nazare@users.noreply.github.com>
2024-07-01 18:37:26 +0530

    Update prereq.sh
    Signed-off-by: Vaibhav-Nazare <Vaibhav.Nazare@ibm.com>

--- src/cloud-api-adaptor/ibmcloud-powervs/image/prereq.sh
+++ src/cloud-api-adaptor/ibmcloud-powervs/image/prereq.sh
@@ -2,15 +2,22 @@
 
 GO_VERSION="1.21.11"
 RUST_VERSION="1.72.0"
-SKOPEO_VERSION="1.5.0"
 
 # Install dependencies
-yum install -y curl protobuf-compiler libseccomp-devel openssl openssl-devel perl skopeo-${SKOPEO_VERSION}
+yum install -y curl libseccomp-devel openssl openssl-devel perl skopeo clang clang-devel
+
+wget https://rpmfind.net/linux/centos-stream/9-stream/CRB/ppc64le/os/Packages/protobuf-compiler-3.14.0-13.el9.ppc64le.rpm
+yum install -y protobuf-compiler-3.14.0-13.el9.ppc64le.rpm
+
+wget https://www.rpmfind.net/linux/centos-stream/9-stream/CRB/ppc64le/os/Packages/device-mapper-devel-1.02.197-2.el9.ppc64le.rpm
+yum install -y device-mapper-devel-1.02.197-2.el9.ppc64le.rpm
 
 # Install Golang
 curl https://dl.google.com/go/go${GO_VERSION}.linux-ppc64le.tar.gz -o go${GO_VERSION}.linux-ppc64le.tar.gz && \
 rm -rf /usr/local/go && tar -C /usr/local -xzf go${GO_VERSION}.linux-ppc64le.tar.gz && \
 rm -f go${GO_VERSION}.linux-ppc64le.tar.gz
+export GOPATH=${HOME}/go
+mkdir -p ${GOPATH}/src/github.com
 
 # Install Rust
 curl https://sh.rustup.rs -sSf | sh -s -- -y --default-toolchain ${RUST_VERSION}
