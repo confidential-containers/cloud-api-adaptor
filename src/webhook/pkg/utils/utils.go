02755db25ba73894af3125c34207487dcc393850
Pradipta Banerjee <pradipta.banerjee@gmail.com>
2024-07-17 22:20:02 +0530

    webhook: Update pod resource spec and add annotation
    Sum all the cpu, mem req/limit, gpu req/limit and set the annotation accordingly.
    
    Also remove the cpu,mem req/limit and gpu req/limit from the container
    resource spec. Add peer-pod resource req/limit
    
    Since the changes are also in utils, took the liberty to remove unused
    methods
    
    Signed-off-by: Pradipta Banerjee <pradipta.banerjee@gmail.com>

--- src/webhook/pkg/utils/utils.go
+++ src/webhook/pkg/utils/utils.go
@@ -1,49 +1,36 @@
 package utils
 
 import (
-	"fmt"
-	"sort"
-	"strings"
+	"strconv"
 
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/resource"
+	"k8s.io/cloud-provider/volume/helpers"
 )
 
 // GetResourceRequestQuantity finds and returns the request quantity for a specific resource.
 func GetResourceRequestQuantity(pod *corev1.Pod, resourceName corev1.ResourceName) resource.Quantity {
-	requestQuantity := resource.Quantity{}
 
-	switch resourceName {
-	case corev1.ResourceCPU:
-		requestQuantity = resource.Quantity{Format: resource.DecimalSI}
-	case corev1.ResourceMemory:
-		requestQuantity = resource.Quantity{Format: resource.BinarySI}
-	default:
-		requestQuantity = resource.Quantity{Format: resource.DecimalSI}
-	}
-
+	requestQuantity := getResourceQuantity(resourceName)
+	// Add the request quantity for each container
 	for _, container := range pod.Spec.Containers {
 		if rQuantity, ok := container.Resources.Requests[resourceName]; ok {
 			requestQuantity.Add(rQuantity)
 		}
 	}
 
+	// Add the request quantity for each init container
 	for _, container := range pod.Spec.InitContainers {
 		if rQuantity, ok := container.Resources.Requests[resourceName]; ok {
-			if requestQuantity.Cmp(rQuantity) < 0 {
-				requestQuantity = rQuantity.DeepCopy()
-			}
+			requestQuantity.Add(rQuantity)
 		}
 	}
 
 	// Don't add PodOverhead to the total requests
-	//if pod.Spec.Overhead != nil {
-	//	if podOverhead, ok := pod.Spec.Overhead[resourceName]; ok && !requestQuantity.IsZero() {
-	//		requestQuantity.Add(podOverhead)
-	//	}
-	//}
+	// as its not needed for peer-pod since the VM is external to the worker
 
 	return requestQuantity
+
 }
 
 // GetResourceRequest finds and returns the request value for a specific resource.
@@ -58,62 +45,43 @@ func GetResourceRequest(pod *corev1.Pod, resource corev1.ResourceName) int64 {
 	return requestQuantity.Value()
 }
 
-// MergePodResourceRequirements merges enumerated requirements with default requirements
-// it annotates the pod with information about what requirements were modified
-func MergePodResourceRequirements(pod *corev1.Pod, defaultRequirements *corev1.ResourceRequirements) {
-	annotations := []string{}
+// GetResourceLimitQuantity finds and returns the limit quantity for a specific resource.
+func GetResourceLimitQuantity(pod *corev1.Pod, resourceName corev1.ResourceName) resource.Quantity {
 
-	for i := range pod.Spec.Containers {
-		annotations = mergeContainerResources(&pod.Spec.Containers[i], defaultRequirements, "container", annotations)
-	}
+	limitQuantity := getResourceQuantity(resourceName)
 
-	for i := range pod.Spec.InitContainers {
-		annotations = mergeContainerResources(&pod.Spec.InitContainers[i], defaultRequirements, "init container", annotations)
-	}
-
-	if len(annotations) > 0 {
-		if pod.ObjectMeta.Annotations == nil {
-			pod.ObjectMeta.Annotations = make(map[string]string)
+	for _, container := range pod.Spec.Containers {
+		if lQuantity, ok := container.Resources.Limits[resourceName]; ok {
+			limitQuantity.Add(lQuantity)
 		}
-		val := strings.Join(annotations, "; ")
-		pod.ObjectMeta.Annotations["kata.peerpods.io/changes"] = val
+	}
+
+	for _, container := range pod.Spec.InitContainers {
+		if lQuantity, ok := container.Resources.Limits[resourceName]; ok {
+			limitQuantity.Add(lQuantity)
+
+		}
+	}
+	return limitQuantity
+}
+
+// Method to get the resource Quantity from the resource name
+func getResourceQuantity(resourceName corev1.ResourceName) resource.Quantity {
+
+	switch resourceName {
+	case corev1.ResourceMemory:
+		return resource.Quantity{Format: resource.BinarySI}
+	default:
+		return resource.Quantity{Format: resource.DecimalSI}
 	}
 }
 
-// mergeContainerResources handles defaulting all of the resources on a container.
-func mergeContainerResources(container *corev1.Container, defaultRequirements *corev1.ResourceRequirements, annotationPrefix string, annotations []string) []string {
-	setRequests := []string{}
-	setLimits := []string{}
-	if container.Resources.Limits == nil {
-		container.Resources.Limits = corev1.ResourceList{}
-	}
-	if container.Resources.Requests == nil {
-		container.Resources.Requests = corev1.ResourceList{}
-	}
+// Method to convert memory quantity to MiB string
+func ConvertMemoryQuantityToMib(memoryQuantity resource.Quantity) (string, error) {
 
-	for k, v := range defaultRequirements.Limits {
-		_, found := container.Resources.Limits[k]
-		if !found {
-			container.Resources.Limits[k] = v.DeepCopy()
-			setLimits = append(setLimits, string(k))
-		}
+	memoryQuantityMib, err := helpers.RoundUpToMiB(memoryQuantity)
+	if err != nil {
+		return "0", err
 	}
-	for k, v := range defaultRequirements.Requests {
-		_, found := container.Resources.Requests[k]
-		if !found {
-			container.Resources.Requests[k] = v.DeepCopy()
-			setRequests = append(setRequests, string(k))
-		}
-	}
-	if len(setRequests) > 0 {
-		sort.Strings(setRequests)
-		a := strings.Join(setRequests, ", ") + fmt.Sprintf(" request for %s %s", annotationPrefix, container.Name)
-		annotations = append(annotations, a)
-	}
-	if len(setLimits) > 0 {
-		sort.Strings(setLimits)
-		a := strings.Join(setLimits, ", ") + fmt.Sprintf(" limit for %s %s", annotationPrefix, container.Name)
-		annotations = append(annotations, a)
-	}
-	return annotations
+	return strconv.FormatInt(memoryQuantityMib, 10), nil
 }
