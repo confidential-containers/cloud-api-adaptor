8c4549699ae1242422ee804b0b80d6497cdd55de
Pradipta Banerjee <pradipta.banerjee@gmail.com>
2024-07-17 22:20:02 +0530

    webhook: Update pod resource spec and add annotation
    Sum all the cpu, mem req/limit, gpu req/limit and set the annotation accordingly.
    
    Also remove the cpu,mem req/limit and gpu req/limit from the container
    resource spec. Add peer-pod resource req/limit
    
    Since the changes are also in utils, took the liberty to remove unused
    methods
    
    Signed-off-by: Pradipta Banerjee <pradipta.banerjee@gmail.com>

--- src/webhook/pkg/mutating_webhook/remove-resourcespec.go
+++ src/webhook/pkg/mutating_webhook/remove-resourcespec.go
@@ -1,7 +1,10 @@
 package mutating_webhook
 
 import (
+	"log"
 	"os"
+
+	"github.com/confidential-containers/cloud-api-adaptor/src/webhook/pkg/utils"
 
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/resource"
@@ -10,7 +13,13 @@ import (
 const (
 	RUNTIME_CLASS_NAME_DEFAULT       = "kata-remote"
 	POD_VM_EXTENDED_RESOURCE_DEFAULT = "kata.peerpods.io/vm"
+	PEERPODS_CPU_ANNOTATION          = "io.katacontainers.config.hypervisor.default_vcpus"
+	PEERPODS_MEMORY_ANNOTATION       = "io.katacontainers.config.hypervisor.default_memory"
+	GPU_RESOURCE_NAME                = "nvidia.com/gpu"
+	PEERPODS_GPU_ANNOTATION          = "kata.peerpods.io.gpus"
 )
+
+var logger = log.New(log.Writer(), "[pod-mutator] ", log.LstdFlags|log.Lmsgprefix)
 
 // mutate POD spec
 // remove the POD resource spec
@@ -26,18 +35,96 @@ func (a *PodMutator) mutatePod(pod *corev1.Pod) (*corev1.Pod, error) {
 		return mpod, nil
 	}
 
-	// Remove all resource specs
-	for idx := range mpod.Spec.Containers {
-		mpod.Spec.Containers[idx].Resources = corev1.ResourceRequirements{}
+	mpod = adjustResourceSpec(mpod)
+
+	return mpod, nil
+}
+
+// function to remove resource spec from the pod spec
+// add the cumulative resources as annotation to pod spec
+// add the peer-pod resource to the first container in the pod spec
+
+func adjustResourceSpec(pod *corev1.Pod) *corev1.Pod {
+
+	// Get total CPU resource requests
+	cpuRequest := utils.GetResourceRequestQuantity(pod, corev1.ResourceCPU)
+
+	// Get total CPU resource limits
+	cpuLimit := utils.GetResourceLimitQuantity(pod, corev1.ResourceCPU)
+
+	// Get total Memory resource requests
+	memoryRequest := utils.GetResourceRequestQuantity(pod, corev1.ResourceMemory)
+
+	// Get total Memory resource limits
+	memoryLimit := utils.GetResourceLimitQuantity(pod, corev1.ResourceMemory)
+
+	// Get total GPU resource requests
+	// GPU resources are always requested in whole numbers and request and limits are same.
+	// So we don't need to check for limits
+	gpuRequest := utils.GetResourceRequestQuantity(pod, corev1.ResourceName(GPU_RESOURCE_NAME))
+
+	// log the resource values
+	logger.Printf("CPU Request: %s, CPU Limit: %s, Memory Request: %s, Memory Limit: %s, GPU Request: %s",
+		cpuRequest.String(), cpuLimit.String(), memoryRequest.String(), memoryLimit.String(), gpuRequest.String())
+
+	// Add the cumulative resources as annotation to pod spec
+	// Use cpuLimit and memoryLimit if those are greater than cpuResource and memoryResource
+	annotations := pod.GetAnnotations()
+	if annotations == nil {
+		annotations = make(map[string]string)
 	}
 
-	for idx := range mpod.Spec.InitContainers {
-		mpod.Spec.InitContainers[idx].Resources = corev1.ResourceRequirements{}
+	// A non-existent request or limit will be 0 and we don't need to add annotation for 0
+	// We only add annotation if the value is greater than 0
+	// Limit will always be preferred over request
+
+	// Add cpu annotation
+	if !cpuRequest.IsZero() && cpuLimit.Cmp(cpuRequest) >= 0 {
+		logger.Printf("Adding CPU annotation based on cpuLimit: %s", cpuLimit.String())
+		annotations[PEERPODS_CPU_ANNOTATION] = cpuLimit.String()
+	} else if cpuRequest.Sign() == 1 {
+		logger.Printf("Adding CPU annotation based on cpuRequest: %s", cpuRequest.String())
+		annotations[PEERPODS_CPU_ANNOTATION] = cpuRequest.String()
+	}
+
+	// Add memory annotation
+	if !memoryRequest.IsZero() && memoryLimit.Cmp(memoryRequest) >= 0 {
+		logger.Printf("Adding Memory annotation based on memoryLimit: %s", memoryLimit.String())
+		memoryLimitMiBStr, err := utils.ConvertMemoryQuantityToMib(memoryLimit)
+		if err != nil {
+			logger.Printf("Error converting memory quantity to MiB: %v", err)
+		}
+		annotations[PEERPODS_MEMORY_ANNOTATION] = memoryLimitMiBStr
+
+	} else if memoryRequest.Sign() == 1 {
+		logger.Printf("Adding Memory annotation based on memoryRequest: %s", memoryRequest.String())
+		memoryRequestMiBStr, err := utils.ConvertMemoryQuantityToMib(memoryRequest)
+		if err != nil {
+			logger.Printf("Error converting memory quantity to MiB: %v", err)
+		}
+		annotations[PEERPODS_MEMORY_ANNOTATION] = memoryRequestMiBStr
+	}
+
+	// Add GPU annotation
+	if gpuRequest.Sign() == 1 {
+		logger.Printf("Adding GPU annotation based on gpuRequest: %s", gpuRequest.String())
+		annotations[PEERPODS_GPU_ANNOTATION] = gpuRequest.String()
+	}
+
+	pod.SetAnnotations(annotations)
+
+	// Remove all resource specs
+	for idx := range pod.Spec.Containers {
+		pod.Spec.Containers[idx].Resources = corev1.ResourceRequirements{}
+	}
+
+	for idx := range pod.Spec.InitContainers {
+		pod.Spec.InitContainers[idx].Resources = corev1.ResourceRequirements{}
 	}
 
 	// Add peer-pod resource to one container
-	mpod.Spec.Containers[0].Resources = defaultContainerResourceRequirements()
-	return mpod, nil
+	pod.Spec.Containers[0].Resources = defaultContainerResourceRequirements()
+	return pod
 }
 
 // defaultContainerResourceRequirements returns the default requirements for a container
