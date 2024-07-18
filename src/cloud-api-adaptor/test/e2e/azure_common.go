2bb986285e930e7d31df029e95bbff689f38ef37
Pradipta Banerjee <pradipta.banerjee@gmail.com>
2024-07-18 08:03:34 +0530

    azure-e2e: Implement GetInstanceType method
    Return the instance size of the pod vm.
    Also create a helper findVM by refactoring existing code.
    
    Signed-off-by: Pradipta Banerjee <pradipta.banerjee@gmail.com>

--- src/cloud-api-adaptor/test/e2e/azure_common.go
+++ src/cloud-api-adaptor/test/e2e/azure_common.go
@@ -9,6 +9,7 @@ import (
 	"testing"
 	"time"
 
+	armcompute "github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/compute/armcompute/v4"
 	pv "github.com/confidential-containers/cloud-api-adaptor/src/cloud-api-adaptor/test/provisioner/azure"
 	log "github.com/sirupsen/logrus"
 )
@@ -18,26 +19,34 @@ type AzureCloudAssert struct{}
 
 var assert = &AzureCloudAssert{}
 
-func checkVMExistence(resourceGroupName, prefixName string) bool {
+// findVM is a helper function to find a VM by its prefix name in a resource group.
+func findVM(resourceGroupName, prefixName string) (*armcompute.VirtualMachine, error) {
 	pager := pv.AzureProps.ManagedVmClient.NewListPager(resourceGroupName, nil)
 
 	for pager.More() {
 		page, err := pager.NextPage(context.Background())
 		if err != nil {
 			log.Errorf("failed to advance page: %v", err)
-			return false
+			return nil, err
 		}
 
 		for _, vm := range page.Value {
 			if strings.HasPrefix(*vm.Name, prefixName) {
 				// VM found
-				return true
+				return vm, nil
 			}
 		}
-
 	}
 
-	return false
+	return nil, nil
+}
+
+func checkVMExistence(resourceGroupName, prefixName string) bool {
+	vm, err := findVM(resourceGroupName, prefixName)
+	if err != nil {
+		return false
+	}
+	return vm != nil
 }
 
 func (c AzureCloudAssert) DefaultTimeout() time.Duration {
@@ -57,5 +66,22 @@ func (c AzureCloudAssert) HasPodVM(t *testing.T, id string) {
 
 func (c AzureCloudAssert) GetInstanceType(t *testing.T, podName string) (string, error) {
 	// Get Instance Type of PodVM
-	return "", nil
+
+	prefixName := "podvm-" + podName
+	vm, err := findVM(pv.AzureProps.ResourceGroupName, prefixName)
+	if err != nil {
+		log.Infof("Virtual machine %s not found in resource group %s", podName, pv.AzureProps.ResourceGroupName)
+		return "", err
+	}
+
+	// VM found
+	// Extract the VM size
+	if vm != nil && vm.Properties != nil && vm.Properties.HardwareProfile != nil {
+		log.Infof("The VM size for VM '%s' is: %s\n", podName, *vm.Properties.HardwareProfile.VMSize)
+		return string(*vm.Properties.HardwareProfile.VMSize), nil
+	} else {
+		log.Errorf("Failed to get VM size for VM '%s'", podName)
+		return "", nil
+	}
+
 }
