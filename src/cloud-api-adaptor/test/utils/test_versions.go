14fc9e59f2f3f683c728d3d7575b1dd445a93fbd
stevenhorsman <steven@uk.ibm.com>
2024-08-13 16:28:36 +0100

    tests/e2e: Add test versions file
    Add a central versions file for all test dependencies
    and initially add our test container image versions
    and the code to process it.
    
    In some e2e tests, particularly the docker-provider ones, when you do multiple
    runs we are likely to hit the rate limits of docker hub, so try switching to the
    coco version of nginx hosted on quay.io,
    [quay mirror](https://hub.docker.com/r/curlimages/curl)
    of the curl container image instead to reduce our usage of docker.io.
    
    Signed-off-by: stevenhorsman <steven@uk.ibm.com>

---
+++ src/cloud-api-adaptor/test/utils/test_versions.go
@@ -0,0 +1,57 @@
+// (C) Copyright Confidential Containers Contributors
+// SPDX-License-Identifier: Apache-2.0
+
+package utils
+
+import (
+	"fmt"
+	"os"
+	"sync"
+
+	"gopkg.in/yaml.v2"
+)
+
+// TestVersions represents the test versions.yaml
+type TestVersions struct {
+	ContainerImage map[string]ContainerImage `yaml:"test_images"`
+}
+
+type ContainerImage struct {
+	Registry string `yaml:"registry"`
+	Tag      string `yaml:"tag"`
+}
+
+func (c ContainerImage) getImage() string {
+	return c.Registry + ":" + c.Tag
+}
+
+// Use singleton to reduce multiple file reads
+var lock = &sync.Mutex{}
+var testVersions *TestVersions
+
+func getTestVersions() (*TestVersions, error) {
+
+	if testVersions == nil {
+		lock.Lock()
+		defer lock.Unlock()
+		// Relative to test/e2e
+		yamlFile, err := os.ReadFile("versions.yaml")
+		if err != nil {
+			return nil, err
+		}
+
+		if err := yaml.Unmarshal(yamlFile, &testVersions); err != nil {
+			return nil, err
+		}
+	}
+
+	return testVersions, nil
+}
+
+func GetImage(image string) (string, error) {
+	test_versions, err := getTestVersions()
+	if err != nil {
+		return "", fmt.Errorf("getTestImage: failed to read image: %s, with error %v", image, err)
+	}
+	return test_versions.ContainerImage[image].getImage(), nil
+}
