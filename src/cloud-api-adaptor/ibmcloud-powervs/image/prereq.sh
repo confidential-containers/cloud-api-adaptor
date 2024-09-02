694877ab0aca3e9471a0db575f22c69549d9eda3
stevenhorsman <steven@uk.ibm.com>
2024-09-02 14:39:13 +0100

    WIP: Remove rust installs
    - Now we aren't building the kata-agent and guest-components, we don't need
    rust installed in the podvm builder
    - TODO update Azure podvm workflow and then remove from the versions.yaml too?

--- src/cloud-api-adaptor/ibmcloud-powervs/image/prereq.sh
+++ src/cloud-api-adaptor/ibmcloud-powervs/image/prereq.sh
@@ -2,7 +2,6 @@
 
 # FIXME to pickup these values from versions.yaml
 GO_VERSION="1.21.12"
-RUST_VERSION="1.75.0"
 SKOPEO_VERSION="1.5.0"
 
 # Install dependencies
@@ -12,7 +11,3 @@ yum install -y curl protobuf-compiler libseccomp-devel openssl openssl-devel per
 curl https://dl.google.com/go/go${GO_VERSION}.linux-ppc64le.tar.gz -o go${GO_VERSION}.linux-ppc64le.tar.gz && \
 rm -rf /usr/local/go && tar -C /usr/local -xzf go${GO_VERSION}.linux-ppc64le.tar.gz && \
 rm -f go${GO_VERSION}.linux-ppc64le.tar.gz
-
-# Install Rust
-curl https://sh.rustup.rs -sSf | sh -s -- -y --default-toolchain ${RUST_VERSION}
-rustup target add powerpc64le-unknown-linux-gnu
