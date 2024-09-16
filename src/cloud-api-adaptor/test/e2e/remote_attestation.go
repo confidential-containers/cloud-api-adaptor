f72109df1ab61ca0ac35fc9d52d8e5737d5262eb
Magnus Kulke <magnuskulke@microsoft.com>
2024-09-16 13:24:24 +0200

    ci: set initdata in remote-attestation test
    Set initdata for remote attestation. allow kbs endpoint to be specified
    for manual test runs.
    
    Signed-off-by: Magnus Kulke <magnuskulke@microsoft.com>

--- src/cloud-api-adaptor/test/e2e/remote_attestation.go
+++ src/cloud-api-adaptor/test/e2e/remote_attestation.go
@@ -1,16 +1,24 @@
 package e2e
 
 import (
+	b64 "encoding/base64"
+	"fmt"
 	"testing"
 
 	"sigs.k8s.io/e2e-framework/pkg/env"
 )
 
 // the test will retrieve a kbs token to verify a successful remote attestation
-func DoTestRemoteAttestation(t *testing.T, e env.Environment, assert CloudAssert) {
+func DoTestRemoteAttestation(t *testing.T, e env.Environment, assert CloudAssert, kbsEndpoint string) {
 	name := "remote-attestation"
 	image := "quay.io/curl/curl:latest"
 	// fail on non 200 code, silent, but output on failure
-	job := NewJob(E2eNamespace, name, 0, image, "curl", "-f", "-s", "-S", "-o", "/dev/null", "http://127.0.0.1:8006/aa/token?token_type=kbs")
+	cmd := []string{"curl", "-f", "-s", "-S", "-o", "/dev/null", "http://127.0.0.1:8006/aa/token?token_type=kbs"}
+	initdata := fmt.Sprintf(testInitdata, kbsEndpoint, kbsEndpoint, kbsEndpoint)
+	b64Data := b64.StdEncoding.EncodeToString([]byte(initdata))
+	annotations := map[string]string{
+		"io.katacontainers.config.runtime.cc_init_data": b64Data,
+	}
+	job := NewJob(E2eNamespace, name, 0, image, WithJobCommand(cmd), WithJobAnnotations(annotations))
 	NewTestCase(t, e, "RemoteAttestation", assert, "Received KBS token").WithJob(job).Run()
 }
