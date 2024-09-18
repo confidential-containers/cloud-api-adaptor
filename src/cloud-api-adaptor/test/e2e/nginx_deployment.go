a3749eeee992e22322dfa81b11745eadc42922b1
Wainer dos Santos Moschetta <wainersm@redhat.com>
2024-09-18 10:09:50 -0300

    test/e2e: ensure Teardown run on failure (DoTestNginxDeployment)
    The teardown function doesn't run if WithSetup fail, which might
    leave the deployment on the cluster and it might mess with next
    tests.
    
    This replaced some t.Fatal() with t.Error() so that the current
    goroutine doesn't exit, in fact, the execution continues after
    the t.Error() call but the test is marked failed. So I had to
    proper return when t.Error().
    
    On WithSetup() failing, it doesn't make sense to run the Assess()
    and the only way to pass the status down to Assess() is through the
    `ctx` variable (due to a limitation on the k8s e2e framework, the `t`
    object is not shared).
    
    Signed-off-by: Wainer dos Santos Moschetta <wainersm@redhat.com>

--- src/cloud-api-adaptor/test/e2e/nginx_deployment.go
+++ src/cloud-api-adaptor/test/e2e/nginx_deployment.go
@@ -103,6 +103,7 @@ func DoTestNginxDeployment(t *testing.T, testEnv env.Environment, assert CloudAs
 
 	nginxImageFeature := features.New("Nginx image deployment test").
 		WithSetup("Create nginx deployment", func(ctx context.Context, t *testing.T, cfg *envconf.Config) context.Context {
+			ctx = context.WithValue(ctx, "WithSetupFailed", false)
 			client, err := cfg.NewClient()
 			if err != nil {
 				t.Fatal(err)
@@ -112,17 +113,28 @@ func DoTestNginxDeployment(t *testing.T, testEnv env.Environment, assert CloudAs
 				t.Fatal(err)
 			}
 			waitForNginxDeploymentAvailable(ctx, t, client, deployment, replicas)
-			t.Log("nginx deployment is available now")
+			if !t.Failed() {
+				t.Log("nginx deployment is available now")
+			} else {
+				ctx = context.WithValue(ctx, "WithSetupFailed", true)
+			}
 			return ctx
 		}).
 		Assess("Access for nginx deployment test", func(ctx context.Context, t *testing.T, cfg *envconf.Config) context.Context {
+			if ctx.Value("WithSetupFailed").(bool) {
+				// Test setup failed, so skip this assess
+				t.Skip()
+				return ctx
+			}
 			client, err := cfg.NewClient()
 			if err != nil {
-				t.Fatal(err)
+				t.Error(err)
+				return ctx
 			}
 			var podlist v1.PodList
 			if err := client.Resources(deployment.ObjectMeta.Namespace).List(ctx, &podlist); err != nil {
-				t.Fatal(err)
+				t.Error(err)
+				return ctx
 			}
 			for _, pod := range podlist.Items {
 				if pod.ObjectMeta.Labels["app"] == "nginx" {
@@ -168,8 +180,10 @@ func waitForNginxDeploymentAvailable(ctx context.Context, t *testing.T, client k
 		return deployObj.Status.AvailableReplicas == rc
 	}), wait.WithTimeout(WAIT_NGINX_DEPLOYMENT_TIMEOUT), wait.WithInterval(10*time.Second)); err != nil {
 		var podlist v1.PodList
+		t.Errorf("%v", err)
 		if err := client.Resources(deployment.ObjectMeta.Namespace).List(ctx, &podlist); err != nil {
-			t.Fatal(err)
+			t.Errorf("%v", err)
+			return
 		}
 		for _, pod := range podlist.Items {
 			if pod.ObjectMeta.Labels["app"] == "nginx" {
@@ -195,6 +209,5 @@ func waitForNginxDeploymentAvailable(ctx context.Context, t *testing.T, client k
 				}
 			}
 		}
-		t.Fatal(err)
 	}
 }
