dec8ae7772ab7b52925931c5edb6e7ec85aff7cf
stevenhorsman <steven@uk.ibm.com>
2024-10-16 11:40:01 +0100

    test/e2e: kbs: Use the cached kbs-client
    To save us storage space, time and the requirement
    on the rust toolchain, we can pull down the cached
    kbs-client rather than building it ourselves.
    
    Update checkout_kbs.sh to use oras to pull the kbs-client
    binary from the cache rather than building it.
    
    Signed-off-by: stevenhorsman <steven@uk.ibm.com>

--- src/cloud-api-adaptor/test/utils/checkout_kbs.sh
+++ src/cloud-api-adaptor/test/utils/checkout_kbs.sh
@@ -13,7 +13,13 @@ VERSIONS_YAML_PATH=$(realpath "${TEST_DIR}/../versions.yaml")
 KBS_REPO=$(yq -e '.git.kbs.url' "${VERSIONS_YAML_PATH}")
 KBS_VERSION=$(yq -e '.git.kbs.reference' "${VERSIONS_YAML_PATH}")
 
-echo "${KBS_REPO}, ${KBS_VERSION}"
+install_kbs_client() {
+    kbs_sha=$1
+    arch=$(uname -m)
+
+    oras pull "ghcr.io/confidential-containers/staged-images/kbs-client:sample_only-${arch}-linux-gnu-${kbs_sha}"
+    chmod +x ./kbs-client
+}
 
 rm -rf "${TEST_DIR}/trustee"
 git clone "${KBS_REPO}" "${TEST_DIR}/trustee"
@@ -21,16 +27,11 @@ pushd "${TEST_DIR}/trustee"
 git checkout "${KBS_VERSION}"
 KBS_SHA="$(git rev-parse HEAD)"
 
-# kbs-client setup - to be removed when we use the cached version instead
-sudo apt-get update -y
-sudo apt-get install -y build-essential pkg-config libssl-dev
-pushd kbs
-make CLI_FEATURES=sample_only cli
-popd
+install_kbs_client "${KBS_SHA}"
 
 pushd kbs/config/kubernetes/base/
 # Trustee only updates their staging image reliably with sha tags,
 # so switch to use that and convert the version to the sha
-kustomize edit set image kbs-container-image=ghcr.io/confidential-containers/staged-images/kbs:${KBS_SHA}
+kustomize edit set image kbs-container-image=ghcr.io/confidential-containers/staged-images/kbs:"${KBS_SHA}"
 # For debugging
 echo "Trustee deployment: $(cat kustomization.yaml). Images: $(grep -A 5 images: kustomization.yaml)"
