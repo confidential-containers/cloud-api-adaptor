dead36c1130905f2cbfd92d5b7a8e409bf0f81b5
Beraldo Leal <bleal@redhat.com>
2025-02-05 17:39:03 -0500

    gcp: rename gcp-zone to region
    Following the previous commits, updated GCP zone naming for consistency:
    
    AWS:   -aws-region
    Azure: -region
    GCP:   -zone
    
    GCP is using zones instead of regions, since IBM is also without prefix,
    I'm joing the non-prefix team here.
    
    Signed-off-by: Beraldo Leal <bleal@redhat.com>

--- src/cloud-providers/gcp/manager.go
+++ src/cloud-providers/gcp/manager.go
@@ -21,7 +21,7 @@ func (_ *Manager) ParseCmd(flags *flag.FlagSet) {
 
 	flags.StringVar(&gcpcfg.GcpCredentials, "gcp-credentials", "", "Google Application Credentials, defaults to `GCP_CREDENTIALS`")
 	flags.StringVar(&gcpcfg.ProjectId, "gcp-project-id", "", "GCP Project ID")
-	flags.StringVar(&gcpcfg.Zone, "gcp-zone", "", "Zone")
+	flags.StringVar(&gcpcfg.Zone, "zone", "", "Zone")
 	flags.StringVar(&gcpcfg.ImageName, "image-name", "", "Pod VM image name")
 	flags.StringVar(&gcpcfg.MachineType, "machine-type", "e2-medium", "Pod VM instance type")
 	flags.StringVar(&gcpcfg.Network, "network", "", "Network ID to be used for the Pod VMs")
