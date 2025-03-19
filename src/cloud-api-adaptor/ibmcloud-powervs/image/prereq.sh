015e55bd382b5c77bbc574797d4cdb923d0bae09
Amulyam24 <amulmek1@in.ibm.com>
2025-03-19 19:13:55 +0530

    ibmcloud-powervs: fix podvm image build
    Update the device-mapper-devel package version to fix the image build.
    
    Fixes: #2338
    
    Signed-off-by: Amulyam24 <amulmek1@in.ibm.com>

--- src/cloud-api-adaptor/ibmcloud-powervs/image/prereq.sh
+++ src/cloud-api-adaptor/ibmcloud-powervs/image/prereq.sh
@@ -9,8 +9,8 @@ yum install -y curl libseccomp-devel openssl openssl-devel skopeo clang clang-de
 wget https://rpmfind.net/linux/centos-stream/9-stream/CRB/ppc64le/os/Packages/protobuf-compiler-3.14.0-13.el9.ppc64le.rpm
 yum install -y protobuf-compiler-3.14.0-13.el9.ppc64le.rpm
 
-wget https://www.rpmfind.net/linux/centos-stream/9-stream/CRB/ppc64le/os/Packages/device-mapper-devel-1.02.197-2.el9.ppc64le.rpm
-yum install -y device-mapper-devel-1.02.197-2.el9.ppc64le.rpm
+wget https://www.rpmfind.net/linux/centos-stream/9-stream/CRB/ppc64le/os/Packages/device-mapper-devel-1.02.202-2.el9.ppc64le.rpm
+yum install -y device-mapper-devel-1.02.202-2.el9.ppc64le.rpm
 
 # Install Golang
 curl https://dl.google.com/go/go${GO_VERSION}.linux-ppc64le.tar.gz -o go${GO_VERSION}.linux-ppc64le.tar.gz && \
