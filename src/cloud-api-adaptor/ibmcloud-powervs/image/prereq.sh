3d2a13bb76aba4afb376192a12a36c950c24283a
Amulyam24 <amulmek1@in.ibm.com>
2025-04-08 11:23:56 +0530

    ibmcloud-powervs: install oras and modify copy-files.sh for image build
    Add command installation for oras in prereq.sh and mody copy-files.sh to add missing
    dependencies.
    
    Fixes: #2365
    
    Signed-off-by: Amulyam24 <amulmek1@in.ibm.com>

--- src/cloud-api-adaptor/ibmcloud-powervs/image/prereq.sh
+++ src/cloud-api-adaptor/ibmcloud-powervs/image/prereq.sh
@@ -2,9 +2,13 @@
 
 # FIXME to pickup these values from versions.yaml
 GO_VERSION="1.23.8"
+ORAS_VERSION="1.2.0"
 
 # Install dependencies
 yum install -y curl libseccomp-devel openssl openssl-devel skopeo clang clang-devel
+
+wget https://github.com/oras-project/oras/releases/download/v${ORAS_VERSION}/oras_${ORAS_VERSION}_linux_ppc64le.tar.gz
+rm -rf /usr/local/bin/oras && tar -C /usr/local/bin -xzf oras_${ORAS_VERSION}_linux_ppc64le.tar.gz && rm -f oras_${ORAS_VERSION}_linux_ppc64le.tar.gz
 
 wget https://rpmfind.net/linux/centos-stream/9-stream/CRB/ppc64le/os/Packages/protobuf-compiler-3.14.0-13.el9.ppc64le.rpm
 yum install -y protobuf-compiler-3.14.0-13.el9.ppc64le.rpm
