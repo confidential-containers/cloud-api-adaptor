f17f1e4c38dcb09bd8c7d4a9df74e53f37515d69
Beraldo Leal <bleal@redhat.com>
2025-06-05 09:15:00 -0400

    gcp: adding ability to bind tags to instances
    This will allow users to set Resource Tags to the podvms. However, since
    GCP works differently from Azure,AWS, we cannot create tags during the
    instance creation, we need to bind. Because of permission required, we
    are assuming those tags will exist at the project level.
    
    Signed-off-by: Beraldo Leal <bleal@redhat.com>

--- src/cloud-providers/gcp/manager.go
+++ src/cloud-providers/gcp/manager.go
@@ -29,6 +29,7 @@ func (_ *Manager) ParseCmd(flags *flag.FlagSet) {
 	flags.BoolVar(&gcpcfg.DisableCVM, "disable-cvm", false, "Use non-CVMs for peer pods")
 	flags.StringVar(&gcpcfg.ConfidentialType, "confidential-type", "", "Used when DisableCVM=false. i.e: TDX, SEV or SEV_SNP. Check if the machine type is compatible.")
 	flags.IntVar(&gcpcfg.RootVolumeSize, "root-volume-size", 10, "Root volume size (in GiB) for the Pod VMs")
+	flags.Var(&gcpcfg.Tags, "tags", "List of tags to be added to the Pod VMs. Tags must already exist in the GCP project. Format: key1=value1,key2=value2")
 }
 
 func (_ *Manager) LoadEnv() {
