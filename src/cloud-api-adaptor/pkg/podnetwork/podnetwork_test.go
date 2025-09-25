022f94867102aa6d1535ac670ab2fe7c110615d8
stevenhorsman <steven@uk.ibm.com>
2025-09-25 13:10:21 +0100

    podnetwork: Add ipv6 mapping to test
    Adjust the string to map from ipv6 to ipv4 to match
    the expected test results
    
    Signed-off-by: Magnus Kulke <magnuskulke@microsoft.com>
    Signed-off-by: stevenhorsman <steven@uk.ibm.com>

--- src/cloud-api-adaptor/pkg/podnetwork/podnetwork_test.go
+++ src/cloud-api-adaptor/pkg/podnetwork/podnetwork_test.go
@@ -105,7 +105,8 @@ func TestWorkerNode(t *testing.T) {
 			require.Equal(t, mockTunnelType, config.TunnelType, "hostInterface=%q", hostInterface)
 
 			require.Equal(t, len(config.Routes), 2, "hostInterface=%q", hostInterface)
-			require.Equal(t, config.Routes[0].Dst.String(), "0.0.0.0/0", "hostInterface=%q", hostInterface)
+			dstRoute := fmt.Sprintf("%s/%d", config.Routes[0].Dst.Addr().Unmap().String(), config.Routes[0].Dst.Bits())
+			require.Equal(t, dstRoute, "0.0.0.0/0", "hostInterface=%q", hostInterface)
 			require.Equal(t, config.Routes[0].GW.String(), "172.16.0.1", "hostInterface=%q", hostInterface)
 			require.Equal(t, config.Routes[0].Dev, "eth0", "hostInterface=%q", hostInterface)
 			require.Equal(t, config.Routes[1].Dst.String(), "172.16.0.0/24", "hostInterface=%q", hostInterface)
