wainersm/cc-cloud-api-adaptor

test/provision: generate the AWS s3 bucket
Instead of using a fixed and shared S3 bucket, let's create one per
execution as it's done with the other resources. Likewise, ensure it's
deleted at end.

Signed-off-by: Wainer dos Santos Moschetta <wainersm@redhat.com>

hack/ci-e2e-aws-cleanup.sh
@@ -81,6 +81,26 @@ delete_amis() {
   fi
 }
 
+delete_s3_buckets() {
+  local tag_bucket="${res_basename}-bucket"
+
+  # List all buckets and find ones that match our naming pattern
+  read -r -a buckets <<< "$(aws s3api list-buckets --query "Buckets[?contains(Name, '${res_basename}')].Name" --output text)"
+
+  if [ ${#buckets[@]} -eq 0 ]; then
+    echo "There aren't S3 buckets to delete."
+    return
+  fi
+
+  for bucket in "${buckets[@]}"; do
+    echo "Deleting S3 bucket: $bucket"
+    # First, delete all objects in the bucket
+    aws s3 rm "s3://$bucket" --recursive 2>/dev/null || true
+    # Then delete the bucket
+    aws s3api delete-bucket --bucket "$bucket" 2>/dev/null || true
+  done
+}
+
 main() {
   TEST_PROVISION_FILE="$(pwd)/aws.properties"
   export TEST_PROVISION_FILE
@@ -94,6 +114,7 @@ main() {
 
   delete_vpcs
   delete_amis
+  delete_s3_buckets
 }
 
 main

confidential-containers/cloud-api-adaptor

hack/ci-e2e-aws-cleanup.sh
@@ -84,6 +84,26 @@ delete_amis() {
   fi
 }
 
+delete_s3_buckets() {
+  local tag_bucket="${res_basename}-bucket"
+
+  # List all buckets and find ones that match our naming pattern
+  read -r -a buckets <<< "$(aws s3api list-buckets --query "Buckets[?contains(Name, '${tag_bucket}')].Name" --output text)"
+
+  if [ ${#buckets[@]} -eq 0 ]; then
+    echo "There aren't S3 buckets to delete."
+    return
+  fi
+
+  for bucket in "${buckets[@]}"; do
+    echo "Deleting S3 bucket: $bucket"
+    # First, delete all objects in the bucket
+    aws s3 rm "s3://$bucket" --recursive 2>/dev/null || true
+    # Then delete the bucket
+    aws s3api delete-bucket --bucket "$bucket" 2>/dev/null || true
+  done
+}
+
 main() {
   TEST_PROVISION_FILE="$(pwd)/aws.properties"
   export TEST_PROVISION_FILE
@@ -97,6 +117,7 @@ main() {
 
   delete_vpcs
   delete_amis
+  delete_s3_buckets
 }
 
 main