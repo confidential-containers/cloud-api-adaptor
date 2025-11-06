6fd8c67a4745d35798c71719f2758818831dc425
Beraldo Leal <bleal@redhat.com>
2025-11-06 10:06:12 -0500

    vsphere: migrate to FlagRegistrar
    Following the same pattern as azure.
    
    Signed-off-by: Beraldo Leal <bleal@redhat.com>

--- src/cloud-providers/vsphere/manager.go
+++ src/cloud-providers/vsphere/manager.go
@@ -18,24 +18,24 @@ func init() {
 }
 
 func (_ *Manager) ParseCmd(flags *flag.FlagSet) {
+	reg := provider.NewFlagRegistrar(flags)
 
-	flags.StringVar(&vspherecfg.VcenterURL, "vcenter-url", "", "URL of vCenter instance to connect to")
-	flags.StringVar(&vspherecfg.UserName, "user-name", "", "vCenter Username")
-	flags.StringVar(&vspherecfg.Password, "password", "", "vCenter Password")
-	flags.StringVar(&vspherecfg.Thumbprint, "thumbprint", "", "SHA1 thumbprint of the vcenter certificate. Enable verification of certificate chain and host name.")
-	flags.StringVar(&vspherecfg.Template, "template", "podvm-template", "vCenter template to deploy")
-	flags.StringVar(&vspherecfg.Datacenter, "data-center", "", "vCenter destination datacenter name")
-	flags.StringVar(&vspherecfg.Datastore, "data-store", "", "vCenter datastore")
-	flags.StringVar(&vspherecfg.Deployfolder, "deploy-folder", "", "vCenter vm destination folder relative to the vm inventory path (your-data-center/vm). \nExample '-deploy-folder peerods' will create or use the existing folder peerpods as the \ndeploy-folder in /datacenter/vm/peerpods")
-	flags.StringVar(&vspherecfg.Cluster, "cluster", "", "vCenter destination cluster name ")
-	flags.StringVar(&vspherecfg.DRS, "drs", "false", "Use DRS for clone placement in destination Vcenter cluster")
-	flags.StringVar(&vspherecfg.Host, "host", "", "vCenter host name of resource pool destination")
+	// Flags with environment variable support
+	reg.StringWithEnv(&vspherecfg.UserName, "user-name", "", "GOVC_USERNAME", "vCenter Username")
+	reg.StringWithEnv(&vspherecfg.Password, "password", "", "GOVC_PASSWORD", "vCenter Password")
+	reg.StringWithEnv(&vspherecfg.Thumbprint, "thumbprint", "", "GOVC_THUMBPRINT", "SHA1 thumbprint of the vcenter certificate. Enable verification of certificate chain and host name.")
+	reg.StringWithEnv(&vspherecfg.VcenterURL, "vcenter-url", "", "GOVC_URL", "URL of vCenter instance to connect to")
+	reg.StringWithEnv(&vspherecfg.Template, "template", "podvm-template", "GOVC_TEMPLATE", "vCenter template to deploy")
+	reg.StringWithEnv(&vspherecfg.Datacenter, "data-center", "", "GOVC_DATACENTER", "vCenter destination datacenter name")
+	reg.StringWithEnv(&vspherecfg.Datastore, "data-store", "", "GOVC_DATASTORE", "vCenter datastore")
+	reg.StringWithEnv(&vspherecfg.Deployfolder, "deploy-folder", "", "GOVC_FOLDER", "vCenter vm destination folder relative to the vm inventory path (your-data-center/vm). \nExample '-deploy-folder peerods' will create or use the existing folder peerpods as the \ndeploy-folder in /datacenter/vm/peerpods")
+	reg.StringWithEnv(&vspherecfg.Cluster, "cluster", "", "GOVC_VCLUSTER", "vCenter destination cluster name ")
+	reg.StringWithEnv(&vspherecfg.DRS, "drs", "false", "GOVC_DRS", "Use DRS for clone placement in destination Vcenter cluster")
+	reg.StringWithEnv(&vspherecfg.Host, "host", "", "GOVC_HOST", "vCenter host name of resource pool destination")
 }
 
 func (_ *Manager) LoadEnv() {
-	provider.DefaultToEnv(&vspherecfg.UserName, "GOVC_USERNAME", "")
-	provider.DefaultToEnv(&vspherecfg.Password, "GOVC_PASSWORD", "")
-	provider.DefaultToEnv(&vspherecfg.Thumbprint, "GOVC_THUMBPRINT", "")
+	// No longer needed - environment variables are handled in ParseCmd
 }
 
 func (_ *Manager) NewProvider() (provider.Provider, error) {
