33abb3736db5c34beffcad045292d22032387429
Beraldo Leal <bleal@redhat.com>
2025-12-15 13:08:48 -0500

    aws: required and secret variables
    Doing the same as previous commit for aws.
    
    Signed-off-by: Beraldo Leal <bleal@redhat.com>

--- src/cloud-providers/aws/manager.go
+++ src/cloud-providers/aws/manager.go
@@ -21,14 +21,14 @@ func (_ *Manager) ParseCmd(flags *flag.FlagSet) {
 	reg := provider.NewFlagRegistrar(flags)
 
 	// Flags with environment variable support
-	reg.StringWithEnv(&awscfg.AccessKeyId, "aws-access-key-id", "", "AWS_ACCESS_KEY_ID", "Access Key ID")
-	reg.StringWithEnv(&awscfg.SecretKey, "aws-secret-key", "", "AWS_SECRET_ACCESS_KEY", "Secret Key")
-	reg.StringWithEnv(&awscfg.SessionToken, "aws-session-token", "", "AWS_SESSION_TOKEN", "Session Token")
+	reg.StringWithEnv(&awscfg.AccessKeyId, "aws-access-key-id", "", "AWS_ACCESS_KEY_ID", "Access Key ID", provider.Secret())
+	reg.StringWithEnv(&awscfg.SecretKey, "aws-secret-key", "", "AWS_SECRET_ACCESS_KEY", "Secret Key", provider.Secret())
+	reg.StringWithEnv(&awscfg.SessionToken, "aws-session-token", "", "AWS_SESSION_TOKEN", "Session Token", provider.Secret())
 	reg.StringWithEnv(&awscfg.InstanceType, "instance-type", "m6a.large", "PODVM_INSTANCE_TYPE", "Pod VM instance type")
 	reg.StringWithEnv(&awscfg.Region, "aws-region", "", "AWS_REGION", "Region")
 	reg.StringWithEnv(&awscfg.LaunchTemplateName, "aws-lt-name", "kata", "PODVM_LAUNCHTEMPLATE_NAME", "AWS Launch Template Name")
 	reg.BoolWithEnv(&awscfg.UseLaunchTemplate, "use-lt", false, "USE_PODVM_LAUNCHTEMPLATE", "Use EC2 Launch Template for the Pod VMs")
-	reg.StringWithEnv(&awscfg.ImageId, "imageid", "", "PODVM_AMI_ID", "Pod VM ami id")
+	reg.StringWithEnv(&awscfg.ImageId, "imageid", "", "PODVM_AMI_ID", "Pod VM ami id", provider.Required())
 	reg.StringWithEnv(&awscfg.KeyName, "keyname", "", "SSH_KP_NAME", "SSH Keypair name to be used with the Pod VM")
 	reg.StringWithEnv(&awscfg.SubnetId, "subnetid", "", "AWS_SUBNET_ID", "Subnet ID to be used for the Pod VMs")
 	reg.BoolWithEnv(&awscfg.UsePublicIP, "use-public-ip", false, "USE_PUBLIC_IP", "Use Public IP for connecting to the kata-agent inside the Pod VM")
