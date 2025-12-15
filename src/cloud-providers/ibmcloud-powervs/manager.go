736709f1c84478f3f64fb449cb970f3a707c5bd4
Beraldo Leal <bleal@redhat.com>
2025-12-15 13:48:52 -0500

    ibmcloud-powervs: required and secret variables
    Doing the same as previous commit.
    
    Signed-off-by: Beraldo Leal <bleal@redhat.com>

--- src/cloud-providers/ibmcloud-powervs/manager.go
+++ src/cloud-providers/ibmcloud-powervs/manager.go
@@ -21,11 +21,11 @@ func (_ *Manager) ParseCmd(flags *flag.FlagSet) {
 	reg := provider.NewFlagRegistrar(flags)
 
 	// Flags with environment variable support
-	reg.StringWithEnv(&ibmcloudPowerVSConfig.ApiKey, "api-key", "", "IBMCLOUD_API_KEY", "IBM Cloud API key")
-	reg.StringWithEnv(&ibmcloudPowerVSConfig.Zone, "zone", "", "POWERVS_ZONE", "PowerVS zone name")
-	reg.StringWithEnv(&ibmcloudPowerVSConfig.ServiceInstanceID, "service-instance-id", "", "POWERVS_SERVICE_INSTANCE_ID", "ID of the PowerVS Service Instance")
-	reg.StringWithEnv(&ibmcloudPowerVSConfig.NetworkID, "network-id", "", "POWERVS_NETWORK_ID", "ID of the network instance")
-	reg.StringWithEnv(&ibmcloudPowerVSConfig.ImageId, "image-id", "", "POWERVS_IMAGE_ID", "ID of the boot image")
+	reg.StringWithEnv(&ibmcloudPowerVSConfig.ApiKey, "api-key", "", "IBMCLOUD_API_KEY", "IBM Cloud API key", provider.Secret(), provider.Required())
+	reg.StringWithEnv(&ibmcloudPowerVSConfig.Zone, "zone", "", "POWERVS_ZONE", "PowerVS zone name", provider.Required())
+	reg.StringWithEnv(&ibmcloudPowerVSConfig.ServiceInstanceID, "service-instance-id", "", "POWERVS_SERVICE_INSTANCE_ID", "ID of the PowerVS Service Instance", provider.Required())
+	reg.StringWithEnv(&ibmcloudPowerVSConfig.NetworkID, "network-id", "", "POWERVS_NETWORK_ID", "ID of the network instance", provider.Required())
+	reg.StringWithEnv(&ibmcloudPowerVSConfig.ImageId, "image-id", "", "POWERVS_IMAGE_ID", "ID of the boot image", provider.Required())
 	reg.StringWithEnv(&ibmcloudPowerVSConfig.SSHKey, "ssh-key", "", "POWERVS_SSH_KEY_NAME", "Name of the SSH Key")
 	reg.StringWithEnv(&ibmcloudPowerVSConfig.ProcessorType, "proc-type", "shared", "POWERVS_PROCESSOR_TYPE", "Name of the processor type")
 	reg.StringWithEnv(&ibmcloudPowerVSConfig.SystemType, "sys-type", "s922", "POWERVS_SYSTEM_TYPE", "Name of the system type")
