a06934c50afc440f5d727e4b6dcf211d51d8adf0
stevenhorsman <steven@uk.ibm.com>
2026-01-12 14:12:22 +0000

    caa: Remove old build logic
    Nothing should be calling the `image-with-arch` make target anymore
    now that the process is unified, so remove it and the code that only it
    called to simplify and remove duplication.
    
    Signed-off-by: stevenhorsman <steven@uk.ibm.com>

--- src/cloud-api-adaptor/hack/build.sh
+++ src/cloud-api-adaptor/hack/build.sh
@@ -26,7 +26,6 @@ fi
 
 supported_arches=${ARCHES:-"linux/amd64"}
 
-arch_file_prefix="${script_dir}/../tags-architectures-"
 arch_prefix="linux/"
 
 # Get a list of comma-separated tags (e.g. latest,5d0da3dc9764-dev), return
@@ -40,8 +39,8 @@ function get_tag_string() {
 
 	# if we are building for a single arch, then also append this to the tag
 	if [[ -n "${arch:-}" ]] && [[ "${arch:-}" != *,* ]];then
-		# strip the `linux/` from the docker buildx platform format
-		arch_suffix="-${arch#"linux/"}"
+		# strip the `linux/` from the docker buildx platform format in the tag
+		arch_suffix="-${arch#${arch_prefix}}"
 	fi
 
 	for tag in ${tags/,/ };do
@@ -75,54 +74,9 @@ function build_caa_payload_image() {
 	popd
 }
 
-function get_arch_specific_tag_string() {
-	local tags="$1"
-	local arch="$2"
-	local tag_string=""
-
-	for tag in ${tags/,/ };do
-		tag_string+=" -t ${registry}/${name}:${tag}-${arch}"
-	done
-
-	echo "$tag_string"
-}
-
-# accept one arch as --platform
-function build_caa_payload_arch_specific() {
-	pushd "${script_dir}/../../"
-
-	arch=${supported_arches#"$arch_prefix"}
-
-	echo "arch="$arch >> "$arch_file_prefix$arch"
-
-	local tag_string
-	local build_type=dev
-
-	tag_string="$(get_arch_specific_tag_string "$dev_tags" "${arch}")"
-	if [[ "$release_build" == "true" ]]; then
-		tag_string="$(get_arch_specific_tag_string "$release_tags" "${arch}")"
-		build_type=release
-	fi
-
-	docker buildx build --platform "${supported_arches}" \
-		--build-arg RELEASE_BUILD="${release_build}" \
-		--build-arg BUILD_TYPE="${build_type}" \
-		--build-arg VERSION="${version}" \
-		--build-arg COMMIT="${commit}" \
-		--build-arg YQ_VERSION="${YQ_VERSION}" \
-		-f cloud-api-adaptor/Dockerfile \
-		${tag_string} \
-		--push \
-		.
-	popd
-}
-
 # Get the options
-while getopts ":ai" option; do
+while getopts ":i" option; do
     case $option in
-        a) # image arch specific
-            build_caa_payload_arch_specific
-            exit;;
         i) # image
             build_caa_payload_image
             exit;;
