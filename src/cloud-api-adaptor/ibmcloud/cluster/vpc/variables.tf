c8a4b5fd1933b0ed823a286ddc34cb1944fd00b1
stevenhorsman <steven@uk.ibm.com>
2026-02-10 16:03:29 +0000

    ibmcloud: Delete self-managed code & doc
    The self-managed cluster creation isn't working properly and hasn't been
    maintained for a while, so delete this code and just point to the ROKS
    setup for ibmcloud.
    
    Signed-off-by: stevenhorsman <steven@uk.ibm.com>

--- src/cloud-api-adaptor/ibmcloud/cluster/vpc/variables.tf
+++
@@ -1,7 +0,0 @@
-#
-# (C) Copyright IBM Corp. 2023.
-# SPDX-License-Identifier: Apache-2.0
-#
-
-variable "cluster_name" {}
-variable "zone" {}
--- src/cloud-api-adaptor/test/provisioner/ibmcloud/provision_initializer.go
+++ src/cloud-api-adaptor/test/provisioner/ibmcloud/provision_initializer.go
@@ -59,9 +59,8 @@ type IBMCloudProperties struct {
 	DedicatedHostIDs      string
 	DedicatedHostGroupIDs string
 
-	WorkerCount   int
-	IsSelfManaged bool
-	DisableCVM    bool
+	WorkerCount int
+	DisableCVM  bool
 
 	VPC        *vpcv1.VpcV1
 	ClusterAPI containerv2.Clusters
@@ -148,10 +147,6 @@ func InitIBMCloudProperties(properties map[string]string) error {
 		} else {
 			IBMCloudProps.WorkerCount = count
 		}
-	}
-	selfManagedStr := properties["IS_SELF_MANAGED_CLUSTER"]
-	if strings.EqualFold(selfManagedStr, "yes") || strings.EqualFold(selfManagedStr, "true") {
-		IBMCloudProps.IsSelfManaged = true
 	}
 	confidentialComputingStr := properties["DISABLECVM"]
 	if strings.EqualFold(confidentialComputingStr, "yes") || strings.EqualFold(confidentialComputingStr, "true") {
