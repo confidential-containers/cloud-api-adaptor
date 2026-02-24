80db3ec34ccfc1fc6713974dd0090fe4c77d7c77
stevenhorsman <steven@uk.ibm.com>
2026-02-24 14:02:15 +0000

    test: Merge conditional assignment
    Fix `QF1007: could merge conditional assignment into variable declaration`
    
    Signed-off-by: stevenhorsman <steven@uk.ibm.com>

--- src/cloud-api-adaptor/test/tools/provisioner-cli/main.go
+++ src/cloud-api-adaptor/test/tools/provisioner-cli/main.go
@@ -36,10 +36,8 @@ func main() {
 	cloudProvider := os.Getenv("CLOUD_PROVIDER")
 	provisionPropsFile := os.Getenv("TEST_PROVISION_FILE")
 	podvmImage := os.Getenv("TEST_PODVM_IMAGE")
-	shouldDeployKbs := false
-	if os.Getenv("DEPLOY_KBS") == "true" || os.Getenv("DEPLOY_KBS") == "yes" {
-		shouldDeployKbs = true
-	}
+	env := os.Getenv("DEPLOY_KBS")
+	shouldDeployKbs := env == "true" || env == "yes"
 
 	installDirectory := os.Getenv("INSTALL_DIR")
 	// If not set assume we are in the test/tools directory
