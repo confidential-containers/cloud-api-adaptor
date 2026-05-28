9e42c8933ce2126eaedc552c116c0b2890119f1d
stevenhorsman <steven@uk.ibm.com>
2026-05-28 12:05:19 +0100

    csi-wrapper: remove csi-wrapper component
    Remove the csi-wrapper component entirely from the codebase.
    This includes all source code, examples, CRDs, and related files.
    
    The csi-wrapper provided CSI volume support for peer pods but is
    no longer needed as part of the project simplification effort.
    
    Signed-off-by: stevenhorsman <steven@uk.ibm.com>
    Assisted-By: IBM Bob

--- src/csi-wrapper/pkg/apis/peerpodvolume/v1alpha1/doc.go
+++
@@ -1,7 +0,0 @@
-// Copyright Confidential Containers Contributors
-// SPDX-License-Identifier: Apache-2.0
-
-// +k8s:deepcopy-gen=package
-// +groupName=confidentialcontainers.org
-
-package v1alpha1
